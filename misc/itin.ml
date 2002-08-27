86843766c794187081d59c3bd3c7aa15f4ecc635
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-08-27 07:20:45 +0000

    + add Itinerary.has_place_ + remove test-case

--- misc/itin.ml
+++ misc/itin.ml
@@ -16,12 +16,13 @@ sig
   val addplace_ : t -> int -> unit
   val get_ : t -> int -> int           (* get an entry in the itinerary, specified by relative age *)
   val hops_to_place_ : t -> int -> int (* throws Failure if place not in itin *)
+  val has_place_ : t -> int -> bool   
   val sub_ : t -> int -> t             (* sub itin up to place specified by int *)
   val unroll_ : t -> t                 
   val shorten_ : aux:t -> main:t -> t  
+  val splice_ : leftitin:t -> rightitin:t -> int -> t (* replaces rightitin's itinerary upto given place with leftitin's *)
   val equal_ : t -> t -> bool          (* semantic equality *)
   val print_ : t -> int -> unit
-  val test_ : unit -> unit    
 end;;
 
 module  Itinerary : Itinerary_t = 
@@ -79,6 +80,10 @@ struct
 	else 
 	  h - 1 (* -1 so that the last node in itinerary is 0 hops away *)
   )
+
+  let has_place_ itin place = 
+    try (ignore (hops_to_place_ itin place); true ) with
+	_ -> false
     
   let print_ itin l = (
     for i = 0 to l - 1 do
@@ -88,8 +93,8 @@ struct
     Printf.printf "\n"; flush stdout
   )
 
-  (* replaces rightitin's itinerary upto given place with leftitin's. Not commutative *)
-  let splice__ ~leftitin ~rightitin p = (
+  
+  let splice_ ~leftitin ~rightitin p = (
     assert (leftitin.graphsize = rightitin.graphsize);
 
     let l1 = hops_to_place_ leftitin p in
@@ -133,15 +138,16 @@ struct
       done;
       match !opt_place with
 	  None -> main
-	| Some integer -> splice__ aux main integer
+	| Some integer -> splice_ aux main integer
   )
 
   let sub_ itin target = (
-    let index = hops_to_place_ itin target + 1 in
-      {cbuf = CircBuf.sub_ itin.cbuf index;
-       graphsize = itin.graphsize;
-       arr = itin.arr;
-       counter = itin.counter}
+
+      let index = hops_to_place_ itin target + 1 in
+	{cbuf = CircBuf.sub_ itin.cbuf index;
+	 graphsize = itin.graphsize;
+	 arr = itin.arr;
+	 counter = itin.counter}
   )
       
 
@@ -193,17 +199,22 @@ struct
 	      revwalk (m - 1)
     )
     in
-      revwalk (CircBuf.length_ itin.cbuf - 1); 
-      let newcb = CircBuf.fromarray_ (LinkedArray.toarray_ larr) in
-      let unroll_len = CircBuf.length_ itin.cbuf in
-      let newarr = Array.make itin.graphsize max_int in
-	CircBuf.iteri_ (fun i place ->  newarr.(place) <- unroll_len - i - 1) newcb;
-
-      (* xxx/slow 2 allocations here. Could make a CircBuf.fromlinkedarray that skips one alloc *)
-      {cbuf=newcb;
-       arr=newarr;
-       counter=unroll_len;
-       graphsize=itin.graphsize}
+      if (length_ itin) = 0 then 
+	make_ ~itinsize:0 ~graphsize:itin.graphsize
+      else (
+	
+	revwalk (CircBuf.length_ itin.cbuf - 1); 
+	let newcb = CircBuf.fromarray_ (LinkedArray.toarray_ larr) in
+	let unroll_len = CircBuf.length_ itin.cbuf in
+	let newarr = Array.make itin.graphsize max_int in
+	  CircBuf.iteri_ (fun i place ->  newarr.(place) <- unroll_len - i - 1) newcb;
+	  
+	  (* xxx/slow 2 allocations here. Could make a CircBuf.fromlinkedarray that skips one alloc *)
+	  {cbuf=newcb;
+	   arr=newarr;
+	   counter=unroll_len;
+	   graphsize=itin.graphsize}
+      )
   )		 
 			    
   let equal_ it1 it2 = (
@@ -224,240 +235,4 @@ struct
   )
       
 
-  let test_ () = (
-
-    let graphsize = ref 5 in
-    let make_itin size input = (
-      let itin = make_ ~itinsize:size ~graphsize:!graphsize in 
-	array_rev_iter (fun x -> addplace_ itin  x) input; itin;
-    ) in
-
-    let test_unrolling size input unrolled_check = (
-      let itin = make_itin size input in
-	let unrolled = unroll_ itin in
-	  assert (maxlength_ unrolled = Array.length unrolled_check);
-	  for i = 0 to Array.length unrolled_check - 1 do
-	    assert ((get_ unrolled i) = unrolled_check.(i));
-	    assert ((hops_to_place_ unrolled unrolled_check.(i)) == i);
-	  done;
-
-    ) in
-
-    (* semantic equality *)
-    assert (equal_ 
-	      (make_itin 4 [| 1; 2; 3; 4|]) 
-	      (make_itin 6 [| 1; 2; 3; 4|])
-	   );
-    assert (not 
-	      (equal_ 
-		 (make_itin 4 [| 1; 2; 3; 4|]) 
-		 (make_itin 4 [| 1; 2; 3|])
-	      )
-	   );
-
-    (* sub itineraries *)
-    assert (equal_ 
-	      (sub_
-		 (make_itin 4 [| 4; 3; 2; 1|])
-		 3)
-	      (make_itin 2 [| 4; 3; 2; 1|])
-	   );
-    assert (equal_ 
-	      (sub_
-		 (make_itin 4 [| 4; 3; 2; 1|])
-		 1)
-	      (make_itin 4 [| 4; 3; 2; 1|])
-	   );	      
-    
-    
-    let itin = make_ ~itinsize:4 ~graphsize:!graphsize in
-      assert (maxlength_ itin = 4);
-      for i = 0 to 3 do
-	try (
-	  ignore (get_ itin i); 
-	  assert false;
-	) with 
-	    Invalid_argument "Itinerary.get_ : No value at this index" -> ();
-      done;
-
-      
-      (* 3-2-1-0 *)
-      for i = 0 to 2 do
-	addplace_ itin i;
-      done;
-      try (
-	assert ((hops_to_place_ itin  3) <> max_int);
-	assert false;
-      ) with 
-	  Failure "Itinerary.hops_to_place_ : place has never been visited" -> ();
-      addplace_ itin 3;
-
-      for i = 0 to 3 do
-	assert ((hops_to_place_ itin  i) =  3 - i);
-	assert (get_ itin (hops_to_place_ itin  i) =  i);
-	assert ((get_ itin i) = (3 - i))
-      done;
-      (* 3-3-3-3 *)
-      for i = 0 to 3 do
-	addplace_ itin 3;
-      done;
-      assert ((hops_to_place_ itin  3) = 0);
-
-      try (
-	assert ((hops_to_place_ itin  2) <> 0);
-	assert false;
-      ) with 
-	  Failure "Itinerary.hops_to_place_ : place visited, but out of itinerary" -> ();
-
-      for i = 0 to 3 do
-	assert ((get_ itin i) =  3)
-      done;
-
-      
-      test_unrolling 4 [|3; 2; 1; 0|] [|3; 2; 1; 0|];
-      test_unrolling 6 [|3; 2; 1; 0|] [|3; 2; 1; 0|];
-
-      test_unrolling 2 [|3; 2; 1; 0|] [|3; 2|];
-
-      test_unrolling 4 [|3; 3; 2; 1|] [|3; 2; 1|];
-      test_unrolling 6 [|3; 3; 2; 1|] [|3; 2; 1|];
-
-      test_unrolling 2 [|3; 3; 2; 1|] [|3|];
-
-      test_unrolling 4 [|3; 3; 3; 2|] [|3; 2|];
-      test_unrolling 6 [|3; 3; 3; 2|] [|3; 2|];
-
-      test_unrolling 4 [|3; 3; 3; 3|] [|3|];
-      test_unrolling 4 [|3; 1; 2; 3|] [|3|];
-
-      test_unrolling 4 [|3; 2; 2; 1|] [|3; 2; 1|];
-      test_unrolling 6 [|3; 2; 2; 1|] [|3; 2; 1|];
-
-      test_unrolling 4 [|3; 1; 1; 3|] [|3|];
-      test_unrolling 6 [|3; 1; 1; 3|] [|3|];
-
-      test_unrolling 2 [|3; 3 |] [|3|];
-      test_unrolling 6 [|3; 3 |] [|3|];
-
-      test_unrolling 1 [|3|] [|3|];
-
-
-      graphsize := 10;
-
-      (* incidentally, shows that current unrolling algorithm is not the best! *)
-      test_unrolling 12  [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 2; 9|] [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9|];
-      test_unrolling 20  [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 2; 9|] [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9|];
-
-      test_unrolling 12  [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 2; 9; 9|] [| 0; 1; 2; 9|];
-      test_unrolling 20  [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 2; 9; 9|] [| 0; 1; 2; 9|];
-
-
-      Printf.printf "Itinerary.test_ : passed \n";
-
-
-    (* Itinerary.shorten_ *)
-    (* 1-2-7  4-5-6-7 *)
-    graphsize := 11;
-    let it1 = make_itin 4 [| 4; 5; 6; 7|] in
-    let it2 = make_itin 3 [| 1; 2; 7|] in
-    let shortened1 = shorten_ ~aux:it1 ~main:it2 
-    and shortened2 = shorten_ ~aux:it2 ~main:it1 in
-      assert (shortened1 = make_itin 3 [| 1; 2; 7|]);
-      assert (shortened2 = make_itin 3 [| 1; 2; 7|]);
-
-   let it1 = make_itin 6 [| 4; 5; 6; 7|] in
-   let it2 = make_itin 7 [| 1; 2; 7|] in
-   let shortened1 = shorten_ ~aux:it1 ~main:it2 in
-   let shortened2 = shorten_ ~aux:it2 ~main:it1 in
-     assert (equal_ shortened1 ( make_itin 3 [| 1; 2; 7|]));
-     assert (equal_ shortened2 ( make_itin 3 [| 1; 2; 7|]));
-
-
-    let it1 = make_itin 9 [|1; 2; 3; 5; 6; 7; 8; 9; 10|] in
-    let it2 = make_itin 6 [|3; 4; 5; 6; 9; 10|] in
-    let shortened1 = shorten_ ~aux:it1 ~main:it2 
-    and shortened2 = shorten_ ~aux:it2 ~main:it1 in
-      assert (shortened1 = make_itin 6 [|3; 4; 5; 6; 9; 10|]);
-      assert (shortened2 = make_itin 6 [|3; 4; 5; 6; 9; 10|]);
-      
-      let it1 = make_itin 12 [|1; 2; 3; 5; 6; 7; 8; 9; 10|] in
-    let it2 = make_itin 6 [|3; 4; 5; 6; 9; 10|] in
-    let shortened1 = shorten_ ~aux:it1 ~main:it2 
-    and shortened2 = shorten_ ~aux:it2 ~main:it1 in
-     assert (equal_ shortened1 ( make_itin 6 [|3; 4; 5; 6; 9; 10|]));
-     assert (equal_ shortened2 ( make_itin 6 [|3; 4; 5; 6; 9; 10|]));
-
-    let it1 = make_itin 9 [|1; 2; 3; 5; 6; 7; 8; 9; 10|] 
-    and it2 = make_itin 9 [|1; 2; 3; 5; 6; 7; 8; 9; 10|] in
-    let shortened1 = shorten_ ~aux:it1 ~main:it2 
-    and shortened2 = shorten_ ~aux:it2 ~main:it1 in
-      assert (shortened1 = it1);
-      assert (shortened2 = it1);
-
-    (* Itinerary.splice__ *)
-    (* splice 3-2-1-0 with 3-2-1-0 at any place -> same itin*)
-    let itin = make_itin 4 [|3; 2; 1; 0|]
-    and itin2 = make_itin 4 [|3; 2; 1; 0|] in 
-    let itin_l = make_itin 5 [|3; 2; 1; 0|]
-    and itin2_l = make_itin 6 [|3; 2; 1; 0|] in 
-      begin
-	for i = 0 to 3 do
-	  assert ((splice__ itin itin2 i) = itin);
-	  assert (maxlength_ (splice__ itin itin2 i) =  4);
-	  assert ((splice__ itin_l itin2_l i) = itin);
-	  assert (maxlength_ (splice__ itin_l itin2_l i) =  4);
-	done;
-      end;
-      
-    (* splice 3-2-3-2 with 3-2-1-0 at 2 or 3 -> same itin*)
-    let itin = make_itin 4 [|3; 2; 3; 2|]
-    and itin2 = make_itin 4 [|3; 2; 1; 0|] in 
-    let itin_l = make_itin 5 [|3; 2; 3; 2|]
-    and itin2_l = make_itin 5 [|3; 2; 1; 0|] in 
-      begin
-	assert ((splice__ itin itin2  2) = itin2);
-	assert (maxlength_ (splice__ itin itin2 2) =  4);
-	assert (maxlength_ (splice__ itin itin2 3) =  4);
-	assert ((splice__ itin itin2  3) = itin2);
-	assert ((splice__ itin_l itin2_l  2) = itin2);
-	assert ((splice__ itin_l itin2_l  3) = itin2);
-	assert (maxlength_ (splice__ itin_l itin2_l 2) =  4);
-	assert (maxlength_ (splice__ itin_l itin2_l 3) =  4);
-      end;
-
-    (* splice 0-0-0-0 with 3-2-1-0 at 0 -> 0*)
-    let itin = make_itin 4 [|0; 0; 0; 0;|]
-    and itin2 = make_itin 4 [|3; 2; 1; 0|] in 
-    let itin_l = make_itin 5 [|0; 0; 0; 0;|]
-    and itin2_l = make_itin 5 [|3; 2; 1; 0|] in 
-      begin
-	let newitin = splice__ itin itin2  0 
-	and newitin_l = splice__ itin_l itin2_l  0 
-	in
-	  assert ((maxlength_ newitin) = 1);
-	  assert ((get_ newitin 0) =  0);
-	  assert ((hops_to_place_ newitin  0) = 0) ;
-	  assert ((maxlength_ newitin_l) = 1);
-	  assert ((get_ newitin_l 0) =  0);
-	  assert ((hops_to_place_ newitin_l  0) = 0) ;
-      end;
-      
-    (* splice 1-3-0 with 3-2-1-0 at  0 -> 1-3-0*)
-    let itin = make_itin 4 [|1; 3; 0|]
-    and itin2 = make_itin 4 [|3; 2; 1; 0|] in 
-      begin
-	let newitin = splice__ itin itin2  0 in
-	  assert ((maxlength_ newitin) = 3);
-	  assert ((get_ newitin 0) = 1);
-	  assert ((hops_to_place_ newitin  1) = 0) ;
-	  assert ((get_ newitin 1) =  3);
-	  assert ((hops_to_place_ newitin 3) = 1) ;
-	  assert ((get_ newitin 2) =  0);
-	  assert ((hops_to_place_ newitin 0) = 2) ;
-      end
-  )
-
 end;;
-
-
-Itinerary.test_ ()
