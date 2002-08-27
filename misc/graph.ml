df2d3c677a9414b39180e17038249b8966091e0a
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-08-27 07:41:50 +0000

    + ported dijkstra routing from oreilly book + added Graph.node_ and Graph.index_ to lookup a node by index and vice-versa + split out test into test/graph-test.ml

--- misc/graph.ml
+++ misc/graph.ml
@@ -21,8 +21,11 @@ sig
   val nhop_neigborsi_ : t -> index:int -> radius:int -> int list
 
   val size_      : t -> int          (* number of nodes in graph, might be <> than max. size of graph *)
+  val index_     : t -> elt -> int
+  val node_      : t -> int -> elt
   val contains_  : t -> elt -> bool
   val containsi_ : t -> int -> bool
+    
 
   val setinfo_   : t -> elt -> int list -> unit (* convenience functions to allow client to associate 'info' with each node *)
   val setinfoi_  : t -> int -> int list -> unit (* Graph module blindly manipulates 'info' *)
@@ -34,6 +37,11 @@ sig
   val add_edge_  : t -> elt -> elt  -> float ->  unit
   val add_edgei_ : t -> int -> int  -> float -> unit
 
+  val route_dij_  : t -> src:elt -> dest:elt -> elt list (* all of these can raise (Failure "No_route") *)
+  val routei_dij_ : t -> src:int -> dest:int -> int list
+  val dist_       : t -> src:elt -> dest:elt -> int
+  val disti_      : t -> src:int -> dest:int -> int
+
   val iteri_ : (int  -> unit) -> t -> unit
   val itern_ : (elt  -> unit) -> t -> unit
   val print_ : t -> unit
@@ -41,18 +49,18 @@ end;;
 
 
 
-module Make(G: GraphType) = 
+module Make(G: GraphType) : S with type elt = G.t = 
 struct
 
-  module Hash = Hashtbl.Make (G)
+  module Hash = Hashtbl.Make (G) 
 
   type elt = G.t
   type cost_t = Nan | Cost of float     
   type adj_mat_t = cost_t array array   
 
   type graphtype_t = Directed | Undirected 
-  type t = { mutable ind : int;  (* index of next new node in nodes array. Equal to size when full *)
-	     size : int;         (* max # nodes in graph *)
+  type t = { mutable ind : int;  (* index of next new node in nodes array. Equal to maxsize when full *)
+	     maxsize : int;         (* max # nodes in graph *)
 	     nodes : elt array;   
 	     info : int list array; (* allows client to store info in the graph, like which walkers are at a node *)
 	     hash : int Hash.t;
@@ -61,15 +69,16 @@ struct
 	   }
 
 
-  let index__ g n = Hash.find g.hash n (* throws Not_found *)
+  let index_ g n = Hash.find g.hash n (* throws Not_found *)
+  let node_ g i = g.nodes.(i)
   
   let make_ node size gtype = {ind = 0; 
-				 size = size;
-				 nodes =  Array.make size node;
-				 info = Array.make size [];
-				 hash =  Hash.create size;
-				 m =  Array.make_matrix size size Nan;
-				 t = gtype}
+			       maxsize = size;
+			       nodes =  Array.make size node;
+			       info = Array.make size [];
+			       hash =  Hash.create size;
+			       m =  Array.make_matrix size size Nan;
+			       t = gtype}
 				  
 				  
   let iteri_ f g = for i = 0 to (g.ind - 1) do f i done
@@ -78,7 +87,7 @@ struct
 
   let size_ g = g.ind
 
-  let contains_ g n = try ignore (index__ g n); true with Not_found -> false
+  let contains_ g n = try ignore (index_ g n); true with Not_found -> false
   let containsi_ g i = g.ind > i
 			 
   let getinfoi_ g i = (
@@ -97,7 +106,7 @@ struct
 
   let getinfo_ g n  = (
     try 
-      let i = index__ g n in 
+      let i = index_ g n in 
 	getinfoi_ g i 
     with
 	Not_found -> raise (Invalid_argument  "Graph.getinfo_: node does not exist");
@@ -110,7 +119,7 @@ struct
 
   let setinfo_ g n info = (
     try 
-      let i = index__ g n in 
+      let i = index_ g n in 
 	setinfoi_ g i info
     with
 	Not_found -> raise (Invalid_argument  "Graph.setinfo_: node does not exist");
@@ -126,7 +135,7 @@ struct
   let add_edge_ g n1 n2 c  = (
     if (n1 = n2) then raise (Invalid_argument "Graph.add_edge_: cannot connect a node to itself");
     try 
-      let x = index__ g n1 and y = index__ g n2 in 
+      let x = index_ g n1 and y = index_ g n2 in 
 	add_edgei_ g x y c
     with 
 	Not_found -> raise (Invalid_argument  "Graph.add_edge_: node does not exist");
@@ -134,7 +143,7 @@ struct
 
 
   let add_node_ g n = (
-    if g.ind = g.size then failwith "add_node: graph is full";
+    if g.ind = g.maxsize then failwith "add_node: graph is full";
     if contains_ g n then raise (Invalid_argument "Graph.add_node_: node already exists")
     else (
       g.nodes.(g.ind) <- n; 
@@ -153,7 +162,7 @@ struct
 
   let neigbors_ g n = (
     try 
-      let i = index__ g n in
+      let i = index_ g n in
 	List.map (fun j -> g.nodes.(j)) (neigborsi_ g i)
     with
 	Not_found -> raise (Invalid_argument "Graph.neigbors_: node does not exist")
@@ -166,6 +175,108 @@ struct
       !res
   )
 
+  let a_cost c = match c with Nan -> false | _ -> true
+  let float_of_cost c = match c with Nan -> failwith "float_of_cost" | Cost x -> x
+
+  exception Found of int
+
+  let add_cost c1 c2 = match (c1, c2) with
+      Cost x, Cost y -> Cost (x +. y)
+    | Nan, Cost y -> c2
+    | Cost x, Nan -> c1
+    | Nan, Nan -> c1
+
+  let less_cost c1 c2 = match (c1, c2) with
+      Cost x, Cost y -> x < y
+    | Cost x, Nan -> true
+    | _, _ -> false
+	
+		     
+  exception No_way
+
+  let routei_dij_ g ~src ~dest = (
+
+    let paths = Array.create (size_ g) (-1)
+    and already_treated = Array.create (size_ g) false
+    and distances = Array.create (size_ g) Nan
+    and nn = (size_ g)
+    and source = src
+    in
+      
+    let first_not_treated () = (
+      try 
+	for i = 0 to nn-1 do if not already_treated.(i) then raise (Found i) done;
+	raise Not_found;
+      with Found i -> i
+    ) in
+    
+
+    let least_not_treated p  = (
+      let index = ref p 
+      and dist = ref distances.(p) in
+	for i = p+1 to nn-1 do
+	  if not already_treated.(i) then 
+	    if less_cost distances.(i) !dist then (
+	      dist := distances.(i);
+	      index := i
+	    )
+	done;
+	!index, !dist;
+    ) in
+				   
+    let one_round g = (
+      let p = first_not_treated () in
+      let np, nc = least_not_treated p in
+	if not(a_cost nc) then raise No_way
+	else (
+	  already_treated.(np) <- true;
+	  for i = 0 to nn-1 do
+	    if not already_treated.(i) then
+	      if a_cost g.m.(np).(i) then 
+		let ic = add_cost distances.(np) g.m.(np).(i) in
+		  if less_cost ic distances.(i) then (
+		    paths.(i) <- np;
+		    distances.(i) <- ic;
+		  )
+	  done;
+	)
+    ) in
+
+			   
+    let dij_ g src = (
+      if containsi_ g src then ( 
+	  
+	  for j=0 to (size_ g - 1) do
+	    let c = g.m.(src).(j) in 
+	      distances.(j) <- c;
+	      if a_cost c then paths.(j) <- src;
+	  done;
+	  try
+	    for k = 0 to nn-2 do ignore (one_round g) done;
+	  with No_way -> ()
+      )
+      else failwith "dij : unknown node";
+      
+    ) in
+      if src = dest then [] else (
+	already_treated.(src) <- true;	
+	dij_ g src;
+	let rec makepath i route = 
+	  if paths.(i) =  src then route else   (makepath paths.(i) route) @ [paths.(i)]
+	in
+	  if not (a_cost distances.(dest)) then raise (Failure "No_route") else
+	    (makepath dest []) @ [dest]
+      )
+  )      
+				   
+  let route_dij_ g ~src ~dest = (
+    let routei = routei_dij_ g (index_ g src) (index_ g dest) in
+      List.map (fun x -> node_ g x) routei
+  )
+				  
+  let dist_ g ~src ~dest = List.length (route_dij_ g src dest)
+  let disti_ g ~src ~dest = List.length (routei_dij_ g src dest)
+    
 
   let nhop_neigborsi_ g ~index ~radius = (
     if not (containsi_ g index) then raise (Invalid_argument "Graph.nhop_neigborsi_: index does not exist");
@@ -173,11 +284,11 @@ struct
     let seen_yet = Array.make (size_ g) false in
     let curngbrs = ref [index] in
       seen_yet.(index) <- true;
-    let step n = (
-      let candidates = neigborsi_list__ g n in
-      let next_hop_ngbrs = ref [] in
-	List.iter (fun n -> 
-		     if not (seen_yet.(n)) then (
+      let step n = (
+	let candidates = neigborsi_list__ g n in
+	let next_hop_ngbrs = ref [] in
+	  List.iter (fun n -> 
+		       if not (seen_yet.(n)) then (
 		       next_hop_ngbrs := n::!next_hop_ngbrs;
 		       seen_yet.(n) <- true;
 		     )
@@ -194,7 +305,7 @@ struct
 
   let nhop_neigbors_ g ~node ~radius = (
     try 
-      let i = index__ g node in
+      let i = index_ g node in
 	List.map (fun j -> g.nodes.(j)) (nhop_neigborsi_ g ~index:i ~radius:radius)
     with
 	Not_found -> raise (Invalid_argument "Graph.nhop_neigbors_: node does not exist")
@@ -209,94 +320,6 @@ struct
 	   ) g
   )
 end;;
-
-module FloatGraphType = 
-struct 
-  type t = float
-  let equal = (=)
-  let hash = Hashtbl.hash
-end;;
-
-module IntGraphType = 
-struct 
-  type t = int
-  let equal = (=)
-  let hash = Hashtbl.hash
-end;;
-
-module FloatGraph = Make(FloatGraphType);;
-module IntGraph = Make(IntGraphType);;
-
-let test_ () = (
-  let module FG = FloatGraph in 
-  let g = FG.make_ 0.0 10 FG.Undirected in 
-    assert (FG.contains_ g 0.0 = false);
-    assert (FG.containsi_ g 0 = false);
-    FG.add_node_ g 0.0;
-    assert (FG.contains_ g 0.0 = true);
-    assert (FG.containsi_ g 0 = true);
-    assert (FG.neigbors_ g 0.0 = []);
-    assert (FG.neigborsi_ g 0 = []);
-    
-    FG.add_node_ g 1.0;
-    FG.add_edgei_ g 0 1 0.0;
-    assert (FG.neigbors_ g 0.0 = [1.0]);
-    assert (FG.neigbors_ g 1.0 = [0.0]);
-    
-    FG.add_node_ g 2.0;
-    FG.add_node_ g 3.0;
-    FG.add_edge_ g 2.0 3.0 0.0;
-    assert (FG.neigborsi_ g 2 = [3]);
-    assert (FG.neigborsi_ g 3 = [2]);
-    
-    FG.add_edge_ g 0.0 2.0 0.0;
-    FG.add_edge_ g 0.0 3.0 0.0;
-    FG.add_edge_ g 2.0 1.0 0.0;
-    FG.add_edge_ g 3.0 1.0 0.0;
-    
-    assert (list_same (FG.neigborsi_list__ g [0; 1; 2; 3]) [0; 1; 2; 3]);
-
-    (* fully connected
-       0 -> 1 2 3 
-       1 -> 0 2 3 
-       2 -> 0 1 3 
-       3 -> 0 1 2 
-    *)
-    let ngbrs = FG.neigborsi_ g 1 
-    and ngbrs_1hop = FG.nhop_neigborsi_ g ~index:1 ~radius:1 
-    and ngbrs_0hop = FG.nhop_neigborsi_ g ~index:1 ~radius:0 
-    and ngbrs_2hop = FG.nhop_neigborsi_ g ~index:1 ~radius:2 in 
-      assert (List.length ngbrs = 3);
-      assert (list_same ngbrs [0; 2; 3]);
-      assert (list_same ngbrs ngbrs_1hop);
-      assert (ngbrs_2hop = []);
-      assert (ngbrs_0hop = [1]);
-
-      let g = FG.make_ 0.0 10 FG.Directed in 
-	FG.add_node_ g 0.0;
-	FG.add_node_ g 1.0;
-	FG.add_node_ g 2.0;
-	FG.add_node_ g 3.0;
-	
-	FG.add_edgei_ g 0 1 0.0;
-	assert (FG.neigborsi_ g 0 = [1]);
-	assert (FG.neigborsi_ g 1 = []);
-
-	assert ((FG.getinfo_ g 0.0) = []);
-	assert ((FG.getinfoi_ g 0) = []);
-	FG.setinfo_ g 0.0 [0;0];
-	FG.setinfoi_ g 1 [1;1];
-	assert ((FG.getinfo_ g 0.0) = [0;0]);
-	assert ((FG.getinfoi_ g 0) = [0;0]);
-	assert ((FG.getinfo_ g 1.0) = [1;1]);
-	assert ((FG.getinfoi_ g 1) = [1;1]);
-	assert ((FG.getinfosi_ g [0;1]) = [0;0;1;1]);
-
-	Printf.printf "Graph.test_ : passed \n";
-)   
-		 
-let _ = test_  ()		 
-
 
 
 
