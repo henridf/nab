c2c6eedcb7a5040bf7666194bc981ee20e0e30f9
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-09-29 18:02:45 +0000

    changes to ler_graphics to accompany rewrite of simulator: coords are now taken as normalized (ie within [0;1]). this way the simulator need not know what the size of the display is, and the graphics module need not know what the size of the topology is.
    also improved the draw_route method

--- misc/ler_graphics.mli
+++ misc/ler_graphics.mli
@@ -1,35 +1,28 @@
-open Ler_utils
 open Coord
-open Graph
 
 val init_gfx : unit -> unit
 val close_gfx : unit -> unit
 val clear_gfx : unit -> unit
 
-val scale_pos : coordi_t -> coordi_t   (* convert 'absolute' coordinates to graphic coordinates *)
-val unscale_pos : coordi_t -> coordi_t (* convert graphic coordinates to 'absolute' coordinates *)
-val scale_posf : coordf_t -> coordi_t   (* convert 'absolute' coordinates to graphic coordinates *)
-val unscale_posf : coordf_t -> coordi_t (* convert graphic coordinates to 'absolute' coordinates *)
-
+val scale_pos : coordf_t -> coordi_t    (* convert 'absolute' coordinates to graphic coordinates *)
+val unscale_pos : coordi_t -> coordf_t  (* convert graphic coordinates to 'absolute' coordinates *)
 
 val draw_grid : int -> unit 
-val ler_draw_segment : coordi_t array -> unit
-val ler_draw_segmentf : (coordf_t) array -> unit
-val ler_draw_segments : coordi_t array -> unit
-val ler_draw_segments_reflect : coordi_t array -> unit
+val ler_draw_segment : coordf_t array -> unit
+val ler_draw_segments : coordf_t array -> unit
+val ler_draw_segments_reflect : coordf_t array -> unit
 
-val draw_nodes : coordi_t array -> unit
-val label_nodes : coordi_t array -> unit
-val draw_and_label_nodes : coordi_t array -> unit
-val circle_nodes : coordi_t array -> float -> unit
-val draw_cross : coordi_t -> int -> unit
+val draw_nodes : coordf_t array -> unit
+val label_node : coordf_t -> string -> unit
+val draw_and_label_nodes : coordf_t array -> unit
+val circle_nodes : coordf_t array -> float -> unit
+val draw_cross : coordf_t -> int -> unit
 
-val draw_route : coordi_t array  -> unit
-val animate_route : coordi_t array -> (coordi_t -> 'a) -> unit
+val draw_route : Coord.coordf_t Route.t  -> unit
 
 val draw_gradient : (coordf_t) array array -> unit
 
-(*val mouse_choose_node : string -> int*)
+val mouse_choose_node :  (coordf_t -> int list) -> string -> int
 
 val dump_window : string -> unit
 
