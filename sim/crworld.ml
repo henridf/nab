35d611d2817f1d7d93f263c5871cca1613db128e
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-10-10 21:20:46 +0000

    convert to class

--- sim/crworld.ml
+++ sim/crworld.ml
@@ -9,137 +9,124 @@ open Misc
 open Common
 
 
-module ContRefTop : Top.Topology_t = 
-  (* This module duplicates code from  contTaurusTop.ml, keep in mind when 
+(* This class duplicates code from  contTaurusTop.ml, keep in mind when 
    changing things *)
-  struct 
 
-    type grid_of_nodes_t = int list array array ref
-    let grid_of_nodes_ = (ref (Array.make_matrix 1 1 []):grid_of_nodes_t)
+class contRefTop : Topology.topology = 
+object(s)
 
-    let gridsize_ = ref 1.0 (* in practice an int, but stored as float to avoid many i2f's*)
-    let center_ = ref [|0.5; 0.5|]
+  val mutable grid_of_nodes_ =  (Array.make_matrix 1 1 [])
+    
+  val mutable gridsize_ =  1.0 (* in practice an int, but stored as float to avoid many i2f's*)
+  val mutable center_ =  [|0.5; 0.5|]
+  val mutable waypoint_targets_ = (Array.make 1 [|0.0; 0.0|])
 
-    (* setup internal structures *)
-    let initialize () = (
-      gridsize_ := round (sqrt (i2f (Param.get Params.nodes)));
-      center_ := [|!gridsize_ /. 2.0; !gridsize_ /. 2.0|];
-      grid_of_nodes_ := (Array.make_matrix (f2i !gridsize_) (f2i !gridsize_) [])
+  initializer (
+    let g = round (sqrt (i2f (Param.get Params.nodes))) in
+    gridsize_ <- g;
+    center_ <- [|g /. 2.0; g /. 2.0|];
+    grid_of_nodes_ <- (Array.make_matrix (f2i g) (f2i g) []);
+    waypoint_targets_ <- (
+      let w = ref (Array.make (Param.get Params.nodes) ([|0.0; 0.0|]:coordf_t)) in
+      Array.iteri (
+	fun i nothing  -> 
+	  !w.(i) <- [|Random.float g; Random.float g|]
+      ) !w;
+      !w
     )
+  )
 
-    let pos_in_grid_ pos = coord_f2i (coord_floor pos)
+  method private pos_in_grid_ pos = coord_f2i (coord_floor pos)
 
-    (* return an initial pos for node i *)
-    let initial_pos i = (
-      let pos = [|Random.float !gridsize_; Random.float !gridsize_|] in
-      let (x, y) = ((pos_in_grid_ pos).(0), (pos_in_grid_ pos).(1)) in
-      pos
-    )
+  (* return an initial pos for node i *)
+  method initial_pos i = (
+    let pos = [|Random.float gridsize_; Random.float gridsize_|] in
+    let (x, y) = ((s#pos_in_grid_ pos).(0), (s#pos_in_grid_ pos).(1)) in
+    pos
+  )
 
-    (* set position internally *)
-    let place_node i pos = (
-      let (x, y) = ((pos_in_grid_ pos).(0), (pos_in_grid_ pos).(1)) in
-      assert (not (List.mem i !grid_of_nodes_.(x).(y)));
-      !grid_of_nodes_.(x).(y) <- i::!grid_of_nodes_.(x).(y);
-    )
+  (* set position internally *)
+  method place_node i pos = (
+    let (x, y) = ((s#pos_in_grid_ pos).(0), (s#pos_in_grid_ pos).(1)) in
+    assert (not (List.mem i grid_of_nodes_.(x).(y)));
+    grid_of_nodes_.(x).(y) <- i::grid_of_nodes_.(x).(y);
+  )
 
-    let reflect_ pos = (
-      let newx = ref (x pos) and newy = ref (y pos) in 
-      if !newx >  !gridsize_ then 
-	newx := (2.0 *. !gridsize_) -. !newx
-      else if !newx < 0.0 then
-	newx := (-1.0) *. !newx;
-      if !newy > !gridsize_  then  
-	newy := (2.0 *. !gridsize_) -. !newy
-      else if !newy < 0.0 then
-	newy := (-1.0) *. !newy;
-      assert (!newx >= 0.0 && !newx <  !gridsize_ && !newy >= 0.0 && !newy <  !gridsize_);
-      [|!newx; !newy|];
-    )
+  method private reflect_ pos = (
+    let newx = ref (x pos) and newy = ref (y pos) in 
+    if !newx >  gridsize_ then 
+      newx := (2.0 *. gridsize_) -. !newx
+    else if !newx < 0.0 then
+      newx := (-1.0) *. !newx;
+    if !newy > gridsize_  then  
+      newy := (2.0 *. gridsize_) -. !newy
+    else if !newy < 0.0 then
+      newy := (-1.0) *. !newy;
+    assert (!newx >= 0.0 && !newx <  gridsize_ && !newy >= 0.0 && !newy <  gridsize_);
+    [|!newx; !newy|];
+  )
+
+  method dist a b = sqrt (Coord.dist_sq a b)
+  method zero_hop_neigbors pos1 pos2 = (coord_round pos1) = (coord_round pos2)
 
 
-    let dist a b = sqrt (Coord.dist_sq a b)
-    let zero_hop_neigbors pos1 pos2 = (coord_round pos1) = (coord_round pos2)
 
-    let (waypoint_move, waypoint_init) = (
+  method private waypoint_move_ data ~index  =  (
+    
+    let target = waypoint_targets_.(index) in
+    let pos = data.pos.(index) in
+    if (s#dist target pos) <= 1.0 then 
+      (* arrived at target *)
+      begin
+	waypoint_targets_.(index) <- [|Random.float gridsize_; Random.float gridsize_|];
+	target
+      end
+    else 
+      begin
+	let direction = normalize (target ---. pos) in
+	pos +++. direction;
+      end
+  )
+    
+  method next_position data ~index ~mob = (
+    match mob with
+      | RANDOMWALK -> 
+	  s#reflect_ (
+	    data.pos.(index) +++. ([|Random.float 2.0; Random.float 2.0|] ---. [|1.0; 1.0|])
+	  )
+      | WAYPOINT -> s#waypoint_move_ data ~index:index
+  )
 
-      let init_done = ref false in
-      let waypoint_targets = ref (Array.make 1 ([|0.0; 0.0|]:coordf_t)) in
-      
-      let init () = (
-	waypoint_targets := (Array.make (Param.get Params.nodes) ([|0.0; 0.0|]:coordf_t));
-	Array.iteri (
-	  fun i nothing  -> 
-	    !waypoint_targets.(i) <- [|Random.float !gridsize_; Random.float !gridsize_|]
-	) !waypoint_targets;
-      ) in
-	
-      let  move data ~index  =  (
+  method update_pos ~index ~oldpos ~newpos = (
+    let (oldx, oldy) = ((s#pos_in_grid_ oldpos).(0), (s#pos_in_grid_ oldpos).(1)) in
+    let (newx, newy) = ((s#pos_in_grid_ newpos).(0), (s#pos_in_grid_ newpos).(1)) in
+    assert (List.mem index (grid_of_nodes_.(oldx).(oldy)));
+    grid_of_nodes_.(oldx).(oldy) <- list_without grid_of_nodes_.(oldx).(oldy) index;      
+    grid_of_nodes_.(newx).(newy) <- index::grid_of_nodes_.(newx).(newy);      
+  )
+    
+  method get_nodes_within_radius data ~node ~radius = (
+    
+    let radius_sq = radius ** 2.0 in
+    let center = data.pos.(node) in
+    let l = ref [] in
+    Array.iteri (fun node pos -> if s#dist center pos <= radius then l := node::!l) data.pos;
+    !l
+  ) 
 
-	if (!init_done = false)  then (
-	  (* need to setup waypoint targets the very first time *)
-	  init();
-	  init_done := true;
-	);
+  (* for now, route in continuous domain is direct path *)
+  method route data ~src ~dst = [data.pos.(src)]
 
-      	let target = !waypoint_targets.(index) in
-	let pos = data.pos.(index) in
-	if (dist target pos) <= 1.0 then 
-	  (* arrived at target *)
-	  begin
-	    !waypoint_targets.(index) <- [|Random.float !gridsize_; Random.float !gridsize_|];
-	    target
-	  end
-	else 
-	  begin
-	    let direction = normalize (target ---. pos) in
-	    pos +++. direction;
-	  end
-      )
+  method scale_unit f = f /. gridsize_
 
-      in
-      (move, init)
-    )
+  method project_2d coord =  Array.map (fun x -> s#scale_unit x) coord 
 
-    let next_position data ~index ~mob = (
-      match mob with
-	| RANDOMWALK -> 
-	    reflect_ (
-	      data.pos.(index) +++. ([|Random.float 2.0; Random.float 2.0|] ---. [|1.0; 1.0|])
-	    )
-	| WAYPOINT -> waypoint_move data ~index:index
-    )
+  method get_nodes_at pos = 
+    let scaleup = pos ***. gridsize_ in
+    let (x,y) = ((s#pos_in_grid_ scaleup).(0), (s#pos_in_grid_ scaleup).(1)) in
+    grid_of_nodes_.(x).(y)
 
-    let update_pos ~index ~oldpos ~newpos = (
-      let (oldx, oldy) = ((pos_in_grid_ oldpos).(0), (pos_in_grid_ oldpos).(1)) in
-      let (newx, newy) = ((pos_in_grid_ newpos).(0), (pos_in_grid_ newpos).(1)) in
-      assert (List.mem index (!grid_of_nodes_.(oldx).(oldy)));
-      !grid_of_nodes_.(oldx).(oldy) <- list_without !grid_of_nodes_.(oldx).(oldy) index;      
-      !grid_of_nodes_.(newx).(newy) <- index::!grid_of_nodes_.(newx).(newy);      
-    )
-     
-    let get_nodes_within_radius data ~node ~radius = (
-      
-      let radius_sq = radius ** 2.0 in
-      let center = data.pos.(node) in
-      let l = ref [] in
-      Array.iteri (fun node pos -> if dist center pos <= radius then l := node::!l) data.pos;
-      !l
-    ) 
-
-    (* for now, route in continuous domain is direct path *)
-    let route data ~src ~dst = [data.pos.(src)]
-
-    let scale_unit f = f /. !gridsize_
-
-    let project_2d coord =  Array.map (fun x -> scale_unit x) coord 
-
-    let get_nodes_at pos = 
-      let scaleup = pos ***. !gridsize_ in
-      let (x,y) = ((pos_in_grid_ scaleup).(0), (pos_in_grid_ scaleup).(1)) in
-      !grid_of_nodes_.(x).(y)
-
-    let sprint_info () = Printf.sprintf "\tGridsize:\t\t\t %f\n" !gridsize_
+  method sprint_info () = Printf.sprintf "\tGridsize:\t\t\t %f\n" gridsize_
 
 end
 
--- sim/discrTorusTop.ml
+++ sim/discrTorusTop.ml
@@ -9,73 +9,61 @@ open Coord
 open Graph
 open Common
 
-module DiscrTorusTop : Top.Topology_t = 
-  (* This module duplicates code from  discrRefTop.ml, keep in mind when 
-   changing things *)
-  struct
+class discrTorusTop : Topology.topology = 
+  object(s)
 
-    let g = ref (Graph.make_wrap_lattice_ ~dim:2 ~side:4)
-    let gridsize_ = ref 1
-    let center_ = ref [|1; 1|]
+  val mutable graph = Graph.make_wrap_lattice_ ~dim:2 ~side:4
+  val mutable gridsize_ =  1
+  val mutable center_ = [|1; 1|]
+  val mutable waypoint_targets_ = (Array.make 1 [|0.0; 0.0|])
 
-    (* Functions that implement the signature *)
-    let initialize () = (
-      gridsize_ := f2i (round (sqrt (i2f (Param.get Params.nodes))));
-      center_ := [| !gridsize_ / 2; !gridsize_/2|];
-      g := Graph.make_wrap_lattice_ ~side:!gridsize_ ~dim:2;
-    )
-
-    let initial_pos i = (
-      let pos = [|Random.int !gridsize_; Random.int !gridsize_|] in
-      coord_i2f pos
-    )
-
-    let place_node i pos = (
-      let posi = coord_f2i pos in
-      assert (not (List.mem i (Graph.getinfo_ !g posi)));
-      Graph.appendinfo_ !g posi i;
-    )
-
-    let get_nodes_within_radius data ~node ~radius = (
-      let neigbor_points = Graph.nhop_and_less_neigbors_ !g ~node:(coord_f2i data.pos.(node)) ~radius:(f2i radius) in
-      List.concat (List.map (fun x -> Graph.getinfo_ !g x) neigbor_points)
-    )
-      
-    let dist a b = 
-      i2f (Graph.lattice_dist_ !g ~src:(coord_f2i a) ~dest:(coord_f2i b))
-	
-    let zero_hop_neigbors coord1 coord2 = (coord1 = coord2)
-      
-  let (waypoint_move, waypoint_init) = (
-
-    let init_done = ref false in
-    let waypoint_targets = ref (Array.make 1 ([|0.0;0.0|]:coordf_t)) in
-    
-    let init () = (
-      waypoint_targets := (Array.make (Param.get Params.nodes) ([|0.0;0.0|]:coordf_t));
+  initializer (
+    let g = f2i (round (sqrt (i2f (Param.get Params.nodes)))) in
+    gridsize_ <- g;
+    center_ <- [| gridsize_ / 2; gridsize_/2|];
+    graph <- Graph.make_wrap_lattice_ ~side:gridsize_ ~dim:2;
+    waypoint_targets_ <- (
+      let w = ref (Array.make (Param.get Params.nodes) ([|0.0; 0.0|]:coordf_t)) in
       Array.iteri (
 	fun i nothing  -> 
-	  !waypoint_targets.(i) <- coord_i2f [|Random.int !gridsize_; Random.int !gridsize_|]
-      ) !waypoint_targets;
-    ) in
+	  !w.(i) <- coord_i2f [|Random.int g; Random.int g|]
+      ) !w;
+      !w
+    )
+  )
     
-    let move data ~index  =  (
-
-	if (!init_done = false)  then (
-	  (* need to setup waypoint targets the very first time *)
-	  init();
-	  init_done := true;
-	);
-
-      let target = !waypoint_targets.(index) in
+  method initial_pos i = (
+    let pos = [|Random.int gridsize_; Random.int gridsize_|] in
+    coord_i2f pos
+  )
+    
+  method place_node i pos = (
+    let posi = coord_f2i pos in
+    assert (not (List.mem i (Graph.getinfo_ graph posi)));
+    Graph.appendinfo_ graph posi i;
+  )
+    
+  method get_nodes_within_radius data ~node ~radius = (
+    let neigbor_points = Graph.nhop_and_less_neigbors_ graph ~node:(coord_f2i data.pos.(node)) ~radius:(f2i radius) in
+    List.concat (List.map (fun x -> Graph.getinfo_ graph x) neigbor_points)
+  )
+    
+  method dist a b = 
+    i2f (Graph.lattice_dist_ graph ~src:(coord_f2i a) ~dest:(coord_f2i b))
+      
+  method zero_hop_neigbors coord1 coord2 = (coord1 = coord2)
+    
+  method private waypoint_move_ data ~index  =  (
+      
+      let target = waypoint_targets_.(index) in
       let pos = data.pos.(index) in
-      if (dist target pos) <= 1.0 then 
+      if (s#dist target pos) <= 1.0 then 
 	(* arrived at target *)
 	begin
-	  !waypoint_targets.(index) <- coord_i2f [|Random.int !gridsize_; Random.int !gridsize_|];
+	  waypoint_targets_.(index) <- coord_i2f [|Random.int gridsize_; Random.int gridsize_|];
 	  if index = 0 then (
 	    (* should do a project_2d before drawing *)
-	    Ler_graphics.circle_nodes [|!waypoint_targets.(index)|] 0.02;
+	    Ler_graphics.circle_nodes [|waypoint_targets_.(index)|] 0.02;
 	    Ler_graphics.draw_nodes [|target|];
 	  );
 	  target
@@ -97,45 +85,41 @@ module DiscrTorusTop : Top.Topology_t =
 	end
     )
       
-
-
-  in
-  (move, init)
-)
-
-    let next_position data ~index ~mob = (
+      
+      
+  method next_position data ~index ~mob = (
       match mob with
 	| RANDOMWALK -> 
-	    coord_i2f (rnd_from_list (Graph.neigbors_lattice_ !g (coord_f2i data.pos.(index)) ~side:!gridsize_))
+	    coord_i2f (rnd_from_list (Graph.neigbors_lattice_ graph (coord_f2i data.pos.(index)) ~side:gridsize_))
 	| WAYPOINT -> (
 	    Printf.printf "!*!\nWaypoint model has never been tested in this
       topology. look at the node movement  and make sure it looks ok!\n!*!\n";
-	    waypoint_move data ~index:index
+	    s#waypoint_move_ data ~index:index
 	  )
     )
 
-    let update_pos ~index ~oldpos ~newpos = (
-      assert (List.mem index (Graph.getinfo_ !g (coord_f2i oldpos)));
-      Graph.setinfo_ !g (coord_f2i oldpos) (list_without (Graph.getinfo_ !g (coord_f2i oldpos)) index);
-      Graph.appendinfo_ !g (coord_f2i newpos) index;
+  method update_pos ~index ~oldpos ~newpos = (
+      assert (List.mem index (Graph.getinfo_ graph (coord_f2i oldpos)));
+      Graph.setinfo_ graph (coord_f2i oldpos) (list_without (Graph.getinfo_ graph (coord_f2i oldpos)) index);
+      Graph.appendinfo_ graph (coord_f2i newpos) index;
     )
 
-    let route data ~src ~dst = 
-      (* we can do the coord_f2i because we know that in this topology all 
-	 float coords are integer valued *)
-      List.map 
-	(fun icoord -> Coord.coord_i2f icoord)
-	(Graph.route_dij_ !g 
-	  (Coord.coord_f2i data.pos.(src)) 
-	  (Coord.coord_f2i data.pos.(dst)))
+  method route data ~src ~dst = 
+    (* we can do the coord_f2i because we know that in this topology all 
+       float coords are integer valued *)
+    List.map 
+      (fun icoord -> Coord.coord_i2f icoord)
+      (Graph.route_dij_ graph 
+	(Coord.coord_f2i data.pos.(src)) 
+	(Coord.coord_f2i data.pos.(dst)))
+      
+    method scale_unit f = f /. (i2f gridsize_)
 
-    let scale_unit f = f /. (i2f !gridsize_)
+    method project_2d coord =  Array.map (fun x -> s#scale_unit x) coord 
 
-    let project_2d coord =  Array.map (fun x -> scale_unit x) coord 
+    method get_nodes_at place = Graph.getinfo_ graph (coord_f2i (coord_round (place ***. (i2f gridsize_))))
 
-    let get_nodes_at place = Graph.getinfo_ !g (coord_f2i (coord_round (place ***. (i2f !gridsize_))))
-
-    let sprint_info () = Printf.sprintf "\tGridsize:\t\t\t %d\n" !gridsize_
+    method sprint_info () = Printf.sprintf "\tGridsize:\t\t\t %d\n" gridsize_
 
   end
   
