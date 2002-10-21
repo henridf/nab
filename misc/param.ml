26b319813494cbfa0957daa359755b135bedd02d
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-10-21 19:00:59 +0000

    make default passed at param creation  optional

--- misc/param.ml
+++ misc/param.ml
@@ -1,5 +1,7 @@
+open Printf
+
 type 'a t = {
-  mutable value: 'a;
+  mutable value: 'a option;
   name:string;
   doc:string;
   reader:(string -> 'a);
@@ -8,13 +10,15 @@ type 'a t = {
 
 exception IllegalValue of string
 
-let create ~name ~default ~doc ~reader ~checker =  
+
+let create ~name ~default ~doc ~reader ~checker =  (
+
   {value=default;
   name=name;
   doc=doc;
   reader=reader;
   checker=checker}
-  
+)
     
 let set param value = (
   let _ = (
@@ -24,19 +28,33 @@ let set param value = (
 	  try 
 	    f value 
 	  with  IllegalValue str -> 
-	    raise(Util.Fatal(Printf.sprintf "%s\n" str))
+	    raise(Misc.Fatal(Printf.sprintf "%s\n" str))
 	)
   ) in
-  param.value <- value;
+  param.value <- (Some value);
 )
 
 let strset param string = (
   let value = try 
     param.reader string 
-  with e -> raise (Util.Fatal 
+  with e -> raise (Misc.Fatal 
     (Printf.sprintf "%s is not correct for param %s\n" string param.name))
   in
   set param value
 )
 
-let get param = param.value
+let get param = match param.value with 
+  | None -> raise (Misc.Fatal  
+      (Printf.sprintf "Cannot provide value for param %s which had no default\n"  param.name))
+  | Some value -> value
+
+
+
+
+
+
+
+
+
+
+
