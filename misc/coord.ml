7a8e87ecd1940df62b7491ec50c0d019ffed9483
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-10-21 18:53:53 +0000

    - move some stuff from misc.ml to here - add a few new goodies

--- misc/coord.ml
+++ misc/coord.ml
@@ -1,6 +1,7 @@
 open Misc
-type coordi_t = int array
-type coordf_t = float array
+type 'a coord_t = 'a array
+type coordi_t = int coord_t
+type coordf_t = float coord_t
 
 
 let (+++) (c1:coordi_t) (c2:coordi_t) = Array.mapi (fun i v -> v + c2.(i)) c1
@@ -8,6 +9,9 @@ let (+++.) (c1:coordf_t) (c2:coordf_t) = Array.mapi (fun i v -> v +. c2.(i)) c1
 
 let (---) (c1:coordi_t) (c2:coordi_t) = Array.mapi (fun i v -> v - c2.(i)) c1
 let (---.) c1 c2 = Array.mapi (fun i v -> v -. c2.(i)) c1
+
+let ( *** ) (c1:coordi_t) scalar = Array.mapi (fun i v -> v * scalar) c1
+let ( ***. ) (c1:coordf_t) scalar = Array.mapi (fun i v -> v *. scalar) c1
 
 let (///) (c1:coordi_t) scalar = Array.mapi (fun i v -> v / scalar) c1
 let (///.) (c1:coordf_t) scalar = Array.mapi (fun i v -> v /. scalar) c1
@@ -18,6 +22,7 @@ let coordf2pair c = (c.(0), c.(1))
 let coord_i2f (c1:coordi_t) = Array.map (fun x -> i2f x) c1
 let coord_f2i (c1:coordf_t) = Array.map (fun x -> f2i x) c1
 let coord_round (c1:coordf_t) = Array.map (fun x -> round x) c1
+let coord_floor (c1:coordf_t) = Array.map (fun x -> floor x) c1
 
 let x c = c.(0) 
 let y c = c.(1)
@@ -67,3 +72,21 @@ let normalize p =
     | 0.0 -> p
     | norm -> p ///. norm
 
+let print p = (
+  Printf.printf "<"; 
+  Array.iter (fun x -> Printf.printf "%d  " x) p;
+  Printf.printf ">"; 
+  flush stdout
+)
+
+let sprint p = (
+  Printf.sprintf "<" ^
+  (Array.fold_left (fun s x -> s ^ (Printf.sprintf "%d " x) ) "" p) ^
+  Printf.sprintf ">"; 
+)
+
+let sprintf p = (
+  Printf.sprintf "<" ^
+  (Array.fold_left (fun s x -> s ^ (Printf.sprintf "%.2f " x) ) "" p) ^
+  Printf.sprintf ">"; 
+)
