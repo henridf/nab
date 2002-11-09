6fcea2640fb812def9c6bf854803e41d20181881
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-11-09 10:59:32 +0000

    use a list to keep track of neighbors, not a set (long time no check, this was done ages ago) : support for packet sending and hooks.

--- sim/node.mli
+++ sim/node.mli
@@ -7,20 +7,17 @@ type node_state_t = {
   node_pos : Coord.coordf_t;
   db_state : NodeDB.nodeDB_state_t
 }
-
-
-
-
+    
 class type node_t =
 	  
 object
   val id : Common.nodeid_t
-
-  val mutable neighbors : Common.NodeSet.t
+    
+  val mutable neighbors : Common.nodeid_t list
   val mutable pos : Coord.coordf_t
-
+    
   method objdescr : string
-
+    
   method id : Common.nodeid_t
   method pos : Coord.coordf_t
   method x : float
@@ -32,10 +29,36 @@ object
   method add_neighbor : node_t -> unit
   method lose_neighbor : node_t -> unit
   method is_neighbor : node_t -> bool
-  method neighbors : Common.NodeSet.t
+  method neighbors : Common.nodeid_t list
     
   method move : Coord.coordf_t -> unit
-  method recv_pkt : pkt:Packet.packet_t -> unit
+
+  method mac_recv_pkt : l2pkt:Packet.l2packet_t -> unit
+  method mac_send_pkt : l3pkt:Packet.packet_t -> mac_dst:node_t -> unit
+    (* Sent packet to neighbor mac_dst. May raise Failure if mac_dst is not a neighbor *)
+
+  method cheat_send_pkt : l3pkt:Packet.packet_t -> dst:node_t -> unit
+    (* Same as above except will accept sending pkt to any destination *)
+
+  method mac_bcast_pkt : l3pkt:Packet.packet_t -> unit
+    (* Broadcast packet to all neighbors *)
+
+  method add_recv_pkt_hook : hook:(Packet.packet_t -> unit) -> unit
+    (* Any agent (routing, application, etc) on this node who might
+       receive packets should register through this hook. 
+       Agent can filter based on l3hdr contents.
+    *)
+
+  method add_mhook : hook:(Packet.l2packet_t -> node_t -> unit) -> unit
+    
+  method add_app_send_pkt_hook : hook:(Packet.app_packet_t -> unit) -> unit
+    (* Routing/Protocol agents should hook here to touch packets being sent by 
+       applications. 
+       For example a proactive routing agent would simply look up the next
+       hop, then call mac_send_pkt on this node.
+       Or an on-demand routing agent would put the packet aside and flood its
+       RREQ via mac_bcast_pkt, and then forward the packet via mac_send_pkt
+       once the route is established *)
 
   method dump_state : node_cnt:int -> node_state_t 
 end
