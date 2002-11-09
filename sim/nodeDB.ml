4459c7d5e083d1d0cf2df3ff0a96051042fe112e
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-11-09 10:22:01 +0000

    use array not hash for db, more efficient

--- sim/nodeDB.ml
+++ sim/nodeDB.ml
@@ -11,36 +11,30 @@ object
   method add_encounter : nid:Common.nodeid_t -> enc:Common.enc_t -> unit
   method last_encounter : nid:Common.nodeid_t -> Common.enc_t option
   method encounter_age : nid:Common.nodeid_t -> Common.time_t 
-  method num_encounters : targets:int -> int
+  method num_encounters : int
   method dump_state : node_cnt:int ->  nodeDB_state_t
   method load_state : dbstate:nodeDB_state_t -> unit
 end
 
 
-class nodeDB : nodeDB_t = 
-object(s)
+class nodeDB ~ntargets : nodeDB_t = 
+object(s: #nodeDB_t)
 
-  val hash = Hashtbl.create 1000
+  val enc_arr = Array.make ntargets None
 
   method add_encounter ~nid:nid ~enc:enc = 
-    Hashtbl.replace hash nid enc
+    enc_arr.(nid) <- Some enc
       
-  method last_encounter ~nid =  (
-    try 
-      Some (Hashtbl.find hash nid)
-    with
-	Not_found -> None
-  )
+  method last_encounter ~nid = enc_arr.(nid) 
     
   method encounter_age ~nid = 
     match s#last_encounter ~nid:nid with
 	None -> max_float
       | Some encounter -> Common.enc_age encounter
 
-  method num_encounters ~targets = 
-    Hashtbl.fold 
-      (fun k v c -> c + if k < targets then 1 else 0) 
-      hash 0
+  method num_encounters = 
+    Array.fold_right
+      (fun encopt count  -> count + if encopt <> None then 1 else 0) enc_arr 0
 
   method dump_state ~node_cnt = 
     Array.init node_cnt (fun i -> s#last_encounter i)
--- sim/nodeDB.mli
+++ sim/nodeDB.mli
@@ -1,17 +1,15 @@
 type nodeDB_state_t = Common.enc_t option array 
 class type nodeDB_t = 
 object 
-
+  
   method add_encounter : nid:Common.nodeid_t -> enc:Common.enc_t -> unit
-
+    
   method last_encounter : nid:Common.nodeid_t -> Common.enc_t option
     (* None if never met *)
-
+    
   method encounter_age : nid:Common.nodeid_t -> Common.time_t 
 
-  method num_encounters : targets:int -> int
-    (* Returns the number of encounters that have occurred for @targets first nodes.*)
-
+  method num_encounters : int
 
   method dump_state : node_cnt:int ->  nodeDB_state_t
     (* returns db state for marshalling. Array size will be nodes_cnt *)
@@ -20,4 +18,4 @@ object
 
 end
 
-class nodeDB : nodeDB_t
+class nodeDB : ntargets:int -> nodeDB_t
