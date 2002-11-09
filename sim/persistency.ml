3c18558b1fd2b3fd467a1afd22727c6ddbbb120d
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2002-11-09 10:55:51 +0000

    - add support for ntargets

--- sim/persistency.ml
+++ sim/persistency.ml
@@ -27,32 +27,34 @@ open Printf
 
 type state_hdr_t = {
   node_cnt:int;
+  ntargets:int;
   time:Common.time_t
 }
 
 type sim_state_t = Node.node_state_t array (* size Nodes_cnt * Nodes_cnt *)
   
-let save_state ~node_cnt ~out_chan = (
+let save_state ~node_cnt ~out_chan ~ntargets = (
   let descr = sprintf "mws datafile\n Parameters:\n nodes: %d\n time: %f\n" 
     node_cnt 
     (Common.get_time())
   in
   let state_hdr = {
     node_cnt=node_cnt;
+    ntargets=ntargets;
     time=(Common.get_time())
   }
   in
-  let (enc_array:sim_state_t) = 
+  let (node_states:sim_state_t) = 
     Nodes.map (fun n -> n#dump_state ~node_cnt:node_cnt)
   in
 
-  if (node_cnt <> Array.length enc_array) then 
+  if (node_cnt <> Array.length node_states) then 
     raise (Failure 
       "Persistency.save_state: node_cnt was different than length of Nodes array");
   
   Marshal.to_channel out_chan descr [];
   Marshal.to_channel out_chan state_hdr [];
-  Marshal.to_channel out_chan enc_array [];
+  Marshal.to_channel out_chan node_states [];
 
   Log.log#log_info "Simulation state saved.";
 
@@ -60,14 +62,14 @@ let save_state ~node_cnt ~out_chan = (
 )
 
 
-let read_state ~in_chan = (
+let read_state ~in_chan  = (
 
   (* get bits from channel *)
   let str = (Marshal.from_channel in_chan : string) in
   let hdr = (Marshal.from_channel in_chan : state_hdr_t) in
-  let enc_array = (Marshal.from_channel in_chan : sim_state_t) in
+  let node_states = (Marshal.from_channel in_chan : sim_state_t) in
   
-  if (hdr.node_cnt <> Array.length enc_array) then 
+  if (hdr.node_cnt <> Array.length node_states) then 
     raise (Failure 
       "Persistency.read_state: node_cnt was different than length of Nodes array");
   
@@ -75,25 +77,31 @@ let read_state ~in_chan = (
   Common.set_time hdr.time;
   Param.set Params.nodes hdr.node_cnt;
 
-  Log.log#log_info "Simulation state read in:";
-  Log.log#log_info (sprintf "\t Nodes: %d" hdr.node_cnt);
-  Log.log#log_info (sprintf "\t Time: %f" hdr.time);
+  Log.log#log_notice "Simulation state read in:";
+  Log.log#log_notice (sprintf "\t Nodes: %d" hdr.node_cnt);
+  Log.log#log_notice (sprintf "\t Ntargets: %d" hdr.ntargets);
+  Log.log#log_notice (sprintf "\t Time: %f" hdr.time);
+
 
   (* make node objects out of restored node states *)
   Nodes.set_nodes
     (Array.mapi
       (fun i nodestate -> 
-	let db = (new NodeDB.nodeDB) 
-	and nd = (new Simplenode.simplenode ~pos_init:nodestate.Node.node_pos ~id:i) 
+	let db = (new NodeDB.nodeDB ~ntargets:hdr.ntargets) 
+	and nd = (new Simplenode.simplenode 
+	  ~pos_init:nodestate.Node.node_pos 
+	  ~id:i
+	  ~ntargets:hdr.ntargets
+	) 
 	in
 	db#load_state nodestate.Node.db_state;
 	nd#set_db db;
+	(* bler_agent will register with node *)
+	ignore (new Magic_bler_agent.magic_bler_agent nd);
 	nd
-      ) enc_array);
+      ) node_states);
   
-  (* Nodes call update_pos in their constructor, but when they do so the
-     nodes_array in Nodes is still empty so we need to call update_pos
-     explicitly after *)
+  (* set up initial node position in internal structures of world object *)
   Nodes.iter (fun n -> (Gworld.world())#update_pos ~node:n ~oldpos_opt:None);
   assert ((Gworld.world())#neighbors_consistent);
 
