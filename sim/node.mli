6734354ac3e680f42b7c58f156a5b1f75f282c51
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2003-03-10 07:32:05 +0000

    add support for recv_l2pkt_hook (for hooks which need to see whole l2pkt) add support for control_hook (for manipulating agents from outside, not                               really thought through)
    pass nodeid's rather than node objects to some methods

--- sim/node.mli
+++ sim/node.mli
@@ -8,6 +8,8 @@ type node_state_t = {
   db_state : NodeDB.nodeDB_state_t
 }
     
+type agent_ctrl_t = AGENT_STOP | AGENT_START
+
 class type node_t =
 	  
 object
@@ -34,33 +36,49 @@ object
   method move : Coord.coordf_t -> unit
 
   method mac_recv_pkt : l2pkt:Packet.l2packet_t -> unit
-  method mac_send_pkt : l3pkt:Packet.packet_t -> mac_dst:node_t -> unit
-    (* Sent packet to neighbor mac_dst. May raise Failure if mac_dst is not a neighbor *)
+  method mac_send_pkt : l3pkt:Packet.l3packet_t -> dstid:Common.nodeid_t -> unit
+    (* Sent packet to neighbor mac_dst. Should raise Mac_send_failure if mac_dst is not a neighbor *)
 
-  method cheat_send_pkt : l3pkt:Packet.packet_t -> dst:node_t -> unit
+  method cheat_send_pkt : l3pkt:Packet.l3packet_t -> dstid:Common.nodeid_t -> unit
     (* Same as above except will accept sending pkt to any destination *)
 
-  method mac_bcast_pkt : l3pkt:Packet.packet_t -> unit
+  method mac_bcast_pkt : l3pkt:Packet.l3packet_t -> unit
     (* Broadcast packet to all neighbors *)
 
-  method add_recv_pkt_hook : hook:(Packet.packet_t -> unit) -> unit
+  method add_recv_pkt_hook : hook:(Packet.l3packet_t -> unit) -> unit
     (* Any agent (routing, application, etc) on this node who might
        receive packets should register through this hook. 
        Agent can filter based on l3hdr contents.
     *)
 
+  method add_recv_l2pkt_hook : hook:(Packet.l2packet_t -> unit) -> unit
+    (* Same as add_recv_l3pkt_hook, except the packet comes with L2 hdr still on, for 
+       those agents which might need it.
+    *)
+
   method add_mhook : hook:(Packet.l2packet_t -> node_t -> unit) -> unit
+    (* the mhook (monitoring hook) on a node gets called each time a packet
+       leaves or enters the node. 
+       This squishes the current mhook if any already in place *)
     
-  method add_app_send_pkt_hook : hook:(Packet.app_packet_t -> unit) -> unit
+  method add_control_hook : hook:(agent_ctrl_t -> unit) -> unit
+  method agent_control : action:agent_ctrl_t -> unit
+
+  method add_app_send_pkt_hook : hook:(Packet.l4pld_t -> dst:Common.nodeid_t -> unit) -> unit
     (* Routing/Protocol agents should hook here to touch packets being sent by 
        applications. 
+
        For example a proactive routing agent would simply look up the next
        hop, then call mac_send_pkt on this node.
        Or an on-demand routing agent would put the packet aside and flood its
        RREQ via mac_bcast_pkt, and then forward the packet via mac_send_pkt
-       once the route is established *)
+       once the route is established 
 
-  method originate_app_pkt : dst:node_t -> unit
+       This squishes the current hook if any already in place *)
+
+
+
+  method originate_app_pkt : dstid:Common.nodeid_t -> unit
     (* originates a packet from an application on this node to dst:
        create the packet and shove it down the app_send_pkt_hooks *)
 
