c8864ebc5b362d12c2ec17a21c1d90005fb6a765
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2003-03-11 19:23:42 +0000

    Initial revision

---
+++ sim/sched.ml
@@ -0,0 +1,110 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
+open Printf
+
+type handler_t = (unit -> unit)
+type sched_time_t = | ASAP (* schedule as soon as possible *)
+		    | ALAP (* schedule as late as possible *)
+		    | Time of Common.time_t  (* schedule at given time *)
+
+type event_t = {
+  handler:handler_t;
+  time:Common.time_t;
+}
+
+type eventlist_entry_t = 
+  | Event of event_t
+  | Stop of event_t          (* Inserted by stop_ methods *)
+
+class type scheduler_t = 
+object 
+
+  method run : unit -> unit 
+
+  method objdescr : string
+
+  method sched_in : handler:handler_t -> t:Common.time_t -> unit
+  method sched_at : handler:handler_t -> t:sched_time_t -> unit
+  method sched_event_at : ev:eventlist_entry_t -> unit
+
+  method stop_in :  t:Common.time_t -> unit
+  method stop_at :  t:sched_time_t -> unit
+
+  method private next_event : eventlist_entry_t option 
+end
+
+let event_time ev = 
+  match ev with 
+    | Event e -> e.time
+    | Stop s -> s.time
+
+let compare ev1 ev2 = 
+  (event_time ev1) < (event_time ev2) 
+  
+class schedList : scheduler_t = 
+
+object(s)
+
+  inherit Log.loggable
+
+  val ll = Linkedlist.create()
+
+  initializer (
+    objdescr <- "/sched/list"
+  )
+    
+  method private next_event =  Linkedlist.pophead ~ll:ll 
+    
+  method sched_event_at ~ev = 
+    Linkedlist.insert ~ll:ll ~v:ev ~compare:compare
+
+  method stop_at ~t = (
+    let str = ref "" in (
+
+      match t with 
+	| ASAP -> 
+	    s#sched_event_at (Stop {handler=(fun () -> ()); time=Common.get_time()});
+	| ALAP -> raise (Failure "schedList.sched: ALAP not implemented\n")
+	| Time t -> (
+	    assert (t > Common.get_time());
+	    s#sched_event_at (Stop {handler=(fun () -> ()); time=t});
+	  );
+    );
+  )
+  method stop_in ~t =  s#stop_at (fun () -> ()) (Time (t +. Common.get_time()))
+
+  method sched_at ~handler ~t = (
+    let str = ref "" in (
+
+      match t with 
+	| ASAP -> 
+	    s#sched_event_at (Event {handler=handler; time=Common.get_time()});
+(*	    str := (sprintf "%f (ASAP)" (Common.get_time()));*)
+	| ALAP -> raise (Failure "schedList.sched: ALAP not implemented\n")
+	| Time t -> (
+	    assert (t > Common.get_time());
+	    s#sched_event_at (Event {handler=handler; time=t});
+(*	    str := (sprintf "%f " t);*)
+	  );
+    );
+(*    s#log_debug (sprintf "scheduling event at %s" !str);*)
+  )
+    
+  method sched_in ~handler ~t = s#sched_at handler (Time (t +. Common.get_time()))
+
+  method run () = (
+    match s#next_event with
+      | None -> ()
+      | Some (Stop s) -> 
+	  Common.set_time (s.time);
+      | Some (Event ev) -> (
+	  Common.set_time (ev.time);
+	  ev.handler();
+	  s#run()
+	)
+
+  )
+
+end
