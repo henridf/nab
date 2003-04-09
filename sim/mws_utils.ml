268c12ffeddbe7fd2040b2cf964da1fdd522b457
henri <henri@5e8c0ee2-f522-0410-974c-db354172515d>
2003-04-09 16:15:08 +0000

    constants were off by a factor of 10

--- sim/mws_utils.ml
+++ sim/mws_utils.ml
@@ -1,7 +1,7 @@
 open Misc
 
-let speed_of_light = 10e8
-let bits_per_sec = 10e6
+let speed_of_light = 1e8
+let bits_per_sec = 1e6
 let propdelay p1 p2 = (sqrt (Coord.dist_sq p1 p2)) /. speed_of_light
 let xmitdelay ~bytes = (i2f (bytes * 8)) /. bits_per_sec
   
