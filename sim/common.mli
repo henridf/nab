0229a1c64d8ce1606d6cdfd2336ca17c6c266d7f
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-04-21 11:26:02 +0000

    remove some old types and lint

--- sim/common.mli
+++ sim/common.mli
@@ -1,4 +1,10 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
 open Coord
+
+
 
 type nodeid_t = int
 val nid_bcast : nodeid_t
@@ -7,32 +13,14 @@ type port_t = int
 
 type time_t = float
 
-
 module NodeSet : Set.S with type elt = nodeid_t
 val nodeset_of_list : nodelist:(nodeid_t list) -> NodeSet.t
-
-type route_algorithm = EASE | GREASE | GRADIENT
-val algo_of_string : string -> route_algorithm
-val string_of_algo : route_algorithm -> string
-
-
-type mobility_pattern = RANDOMWALK | WAYPOINT
-val mobility_of_string : string -> mobility_pattern
-val string_of_mobility : mobility_pattern -> string
-
-type topology = DISCRETE | CONTINUOUS_TORUS | CONTINUOUS_REFLECTIVE
-val topology_of_string : string -> topology 
-val string_of_topology : topology -> string
-
-type action = COMPUTE_ROUTES | SHOW_ROUTES | SHOW_GRAD
-val action_of_string : string -> action
-val string_of_action : action -> string
 
 type enc_t = {
   mutable t: time_t; 
   mutable p: coordf_t
 }
-    
+
 val enc : time:time_t -> place:Coord.coordf_t -> enc_t
 val enc_age : enc_t -> time_t
 
