e3a008f92f6f27d4e9abd17ae9135568df3c9e50
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-04-22 12:36:30 +0000

    - don't need a new type (guipos_t) for positions - 2 new signatures for entering positions

--- gui/pos.mli
+++ gui/pos.mli
@@ -1,3 +1,7 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
 (* need some sort of API for gui to get positions.*)
 
 (* this api should be general enough to hide many possible models:
@@ -8,40 +12,40 @@
 *)   
 
 
-
 (* gui only knows positions in pixels *)
 (* translation from meters should be done within the mhooks *)
-(* all work necessary to populate the data structures used by this module
-   should also be done within mhooks *)
-
-
-type guipos_t = Coord.coordi_t
 
 (* returns the node's position at given time *)
 val node_pos_inst : 
   Common.nodeid_t -> 
   Common.time_t -> 
-  Common.time_t * guipos_t
+  Common.time_t * Coord.coordi_t
 
 val node_pos_range : 
   Common.nodeid_t -> 
   start:Common.time_t ->
   finish:Common.time_t ->
-  (Common.time_t * guipos_t) list 
+  (Common.time_t * Coord.coordi_t) list 
   (* why list? array would be no use. since time is not directly dependent on
      teh index, we need to scan 1by1 anyway to get through the list. 
      if ranges become too large for this to be efficient, we should use
      something like a heap or tree or whatever.*)
 
-
 val nodes_pos_inst : 
   Common.nodeid_t array -> 
   Common.time_t -> 
-  (Common.time_t * guipos_t) array
-
+  (Common.time_t * Coord.coordi_t) array
 
 val nodes_pos_range : 
   Common.nodeid_t array -> 
   start:Common.time_t ->
   finish:Common.time_t ->
-  (Common.time_t * guipos_t) list array 
+  (Common.time_t * Coord.coordi_t) list array 
+
+val enter_node_pos : 
+  (Common.nodeid_t * Coord.coordi_t) ->
+  unit
+
+val enter_nodes_pos : 
+  (Common.nodeid_t * Coord.coordi_t) array ->
+  unit
