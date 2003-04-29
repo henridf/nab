4ef8688688e68497ae3cbfe405e01fb73d3a4e96
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-04-29 12:01:55 +0000

    add a little fun to return the center of a box (in pixels)

--- gui/read_coords.ml
+++ gui/read_coords.ml
@@ -1,31 +1,33 @@
 open Graph
+open Coord
 open Misc
 
 let gr = ref None
 let g () = o2v !gr
 
+
 let make_graph() = (
 
-let ic = open_in "/home/henri/work/150th/coordinates.txt" in
+  let ic = open_in "/home/henri/work/150th/coordinates.txt" in
 
-let re = Str.regexp "[ \t]+" in
+  let re = Str.regexp "[ \t]+" in
 
-gr := Some (Graph.make_ "" 114 Graph.Undirected);
+  gr := Some (Graph.make_ "" 114 Graph.Undirected);
 
-begin
-  try 
-    while true do
-      let arr = Array.of_list (Str.split (Str.regexp "[ \t]+") (input_line ic)) in
-      let node = arr.(0) in
+  begin
+    try 
+      while true do
+	let arr = Array.of_list (Str.split (Str.regexp "[ \t]+") (input_line ic)) in
+	let node = arr.(0) in
 	Graph.add_node_ (g()) node;
 	let info = Array.map 
 	  (fun s -> int_of_string s) 
 	  (Array.sub arr 1 8) in
 	Graph.setinfo_ (g()) node (Array.to_list info)
       done
-  with 
-    | End_of_file -> ()
-    | e -> raise e;
+    with 
+      | End_of_file -> ()
+      | e -> raise e;
   end;
 
   seek_in ic 0;
@@ -42,13 +44,23 @@ begin
 	    )
 	  ) arr
       done
-  with 
-    | End_of_file -> ()
-    | e -> raise e
+    with 
+      | End_of_file -> ()
+      | e -> raise e
 
   end;
 
 )
+
+let box_centeri  i = 
+  let pts = Graph.getinfoi_ (g()) i in
+  match pts with 
+    | x1::y1::x2::y2::x3::y3::x4::y4::[] -> 
+	(((x1, y1) +++ (x3, y3)) /// 2 )
+   | _ -> raise (Misc.Impossible_Case "Read_coords.box_center")
+
+
+
 (*
 Check connectedness
   Graph.itern_ (fun src -> 
