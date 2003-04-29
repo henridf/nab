75217cb9c7b07bfee6fe06902dca4145a5d1e3d5
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-04-29 12:11:38 +0000

    + hooks to assemble route

--- gui/gui_hooks.ml
+++ gui/gui_hooks.ml
@@ -12,8 +12,21 @@ and y_mtr() = Param.get Params.y_size
 
 let x_mtr_to_pix x = f2i ((i2f !x_pix_size) *. (x /. x_mtr()))
 let y_mtr_to_pix y = f2i ((i2f !y_pix_size) *. (y /. y_mtr()))
+
+let x_pix_to_mtr x = (x_mtr()) *. ((i2f x) /. (i2f !x_pix_size))
+let y_pix_to_mtr y = (y_mtr()) *. ((i2f y) /. (i2f !y_pix_size))
+
+
 let pos_mtr_to_pix pos = 
   (x_mtr_to_pix (Coord.xx pos), y_mtr_to_pix (Coord.yy pos))
+
+let pos_pix_to_mtr pos = 
+  (x_pix_to_mtr (Coord.xx pos), y_pix_to_mtr (Coord.yy pos))
+
+
+let closest_node_at pix_pos = 
+  let pos = pos_pix_to_mtr pix_pos in
+    (o2v ((Gworld.world())#find_closest ~pos ~f:(fun _ -> true)))
 
 
 let node_moved newpos node = (
@@ -24,3 +37,57 @@ let node_moved newpos node = (
 let attach_mob_hooks()  = 
   Nodes.iter
     (fun n -> n#add_mob_mhook ~hook:node_moved)
+
+
+open Packet
+
+let route_done = ref false
+
+let ease_route_pktin_mhook routeref l2pkt (node:Node.node_t) = (
+
+  let l3dst = (Packet.get_l3hdr l2pkt.l3pkt).dst 
+  and l3src = (Packet.get_l3hdr l2pkt.l3pkt).src in
+
+  match l2pkt.l2hdr.l2src <> node#id with
+    | true -> 	(* Packet arriving at a node *)
+(*	(Log.log)#log_info (Printf.sprintf "Arriving t node %d\n" node#id);	  *)
+
+	if  node#id = l3dst then ( (* Packet arriving at dst. *)
+	  route_done := true;
+	  routeref := Route.add_hop !routeref {
+	    Route.hop=node#pos;
+	    Route.anchor=(Packet.get_l3hdr l2pkt.l3pkt).anchor_pos;
+	    Route.anchor_age=(Packet.get_l3hdr l2pkt.l3pkt).ease_enc_age;
+	    Route.searchcost=0.0; (* hack see general_todo.txt *)
+	  }
+	)
+    | false ->  assert(false)
+)
+
+let ease_route_pktout_mhook routeref l2pkt (node:Node.node_t) = (
+
+  let l3dst = (Packet.get_l3hdr l2pkt.l3pkt).dst 
+  and l3src = (Packet.get_l3hdr l2pkt.l3pkt).src in
+
+  match l2pkt.l2hdr.l2src <> node#id with
+    | true -> 	assert(false)
+    | false ->  (* Packet leaving some node *)
+
+	  (*	  (Log.log)#log_info (Printf.sprintf "Leaving src %d\n" node#id);	  *)
+	  routeref := Route.add_hop !routeref {
+	    Route.hop=node#pos;
+	    Route.anchor=(Packet.get_l3hdr l2pkt.l3pkt).anchor_pos;
+	    Route.anchor_age=(Packet.get_l3hdr l2pkt.l3pkt).ease_enc_age;
+	    Route.searchcost=(Packet.get_l3hdr l2pkt.l3pkt).search_dist;
+	  }
+
+)
+
+let mtr_2_pix_route r = 
+  List.map 
+    (fun h -> 
+      {h with
+	Route.hop=(pos_mtr_to_pix h.Route.hop);
+	Route.anchor=(pos_mtr_to_pix h.Route.anchor)
+	}
+    ) r
--- gui/gui_hooks.mli
+++ gui/gui_hooks.mli
@@ -12,14 +12,44 @@
 (* coordinates: gtk must convert between mws coordinates (meters) and screen coordinates.
    All other parts of mws are screen-unaware and use mws coordinates *)
 
+val x_mtr_to_pix : float -> int
+
+val pos_pix_to_mtr : Coord.coordi_t -> Coord.coordf_t
+val pos_mtr_to_pix : Coord.coordf_t -> Coord.coordi_t
+
+
+(* mws => gui *)
+
 val node_moved : 
   Coord.coordf_t -> 
   Node.node_t -> 
   unit
 
-
 val attach_mob_hooks :
   unit -> 
   unit
 
+(* Nodeid_t Route.t is not possible (as in bler) b/c anchors cannot be
+   represented as nodes *)
+val ease_route_pktin_mhook : 
+  Coord.coordf_t Route.t ref ->
+  Packet.l2packet_t ->
+  Node.node_t -> 
+  unit
 
+val ease_route_pktout_mhook : 
+  Coord.coordf_t Route.t ref ->
+  Packet.l2packet_t ->
+  Node.node_t -> 
+  unit
+
+val route_done : bool ref
+
+val mtr_2_pix_route :
+  Coord.coordf_t Route.t  ->
+  Coord.coordi_t Route.t  
+
+(* gui => mws *)
+val closest_node_at :
+  Coord.coordi_t ->
+    Common.nodeid_t
