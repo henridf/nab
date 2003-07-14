9cd66c4ea346f09b044423384bf828fc6e18eb10
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-07-14 00:57:25 +0000

    unhardcode pix_sizes and get them from Params

--- sim/mob.ml
+++ sim/mob.ml
@@ -6,17 +6,17 @@ open Misc
 
 (* xxx/hack copied from gui_hooks b/c otherwise makefile problems in using
    Gui_hooks.* from here *)
-let x_pix_size = ref 1200
-let y_pix_size = ref 900
+let x_pix_size() = Param.get Params.x_pix_size
+let y_pix_size() = Param.get Params.y_pix_size
 
 let x_mtr() = Param.get Params.x_size
 and y_mtr() = Param.get Params.y_size
 
-let x_mtr_to_pix x = f2i ((i2f !x_pix_size) *. (x /. x_mtr()))
-let y_mtr_to_pix y = f2i ((i2f !y_pix_size) *. (y /. y_mtr()))
+let x_mtr_to_pix x = f2i ((i2f (x_pix_size())) *. (x /. x_mtr()))
+let y_mtr_to_pix y = f2i ((i2f (y_pix_size())) *. (y /. y_mtr()))
 
-let x_pix_to_mtr x = (x_mtr()) *. ((i2f x) /. (i2f !x_pix_size))
-let y_pix_to_mtr y = (y_mtr()) *. ((i2f y) /. (i2f !y_pix_size))
+let x_pix_to_mtr x = (x_mtr()) *. ((i2f x) /. (i2f (x_pix_size())))
+let y_pix_to_mtr y = (y_mtr()) *. ((i2f y) /. (i2f (y_pix_size())))
 
 let pos_mtr_to_pix pos = 
   (x_mtr_to_pix (Coord.xx pos), y_mtr_to_pix (Coord.yy pos))
