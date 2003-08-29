b6d48dc5125b35df0f065e1d33227f74dc994dd8
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-08-29 21:34:17 +0000

    new traffic_generator type and support

---
+++ sim/tsource.ml
@@ -0,0 +1,31 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
+open Misc
+
+type traffic_generator_t = (unit -> float option)
+
+let rndgen = Randoms.create ()
+
+let make_counting_trafficsource f num_pkts = 
+    let ct_ = ref 0 in 
+    let f_ = fun()  -> (
+      incr ct_;   if !ct_ < num_pkts then Some (f())  else None)
+    in
+    f_
+
+
+let make_cbr ~num_pkts ~pkts_per_sec = 
+  let time_to_next_pkt() = 1.0 /. (i2f pkts_per_sec) in
+  make_counting_trafficsource time_to_next_pkt num_pkts
+  
+    
+    
+let make_poisson ~num_pkts ~lambda =
+  let time_to_next_pkt() = 
+    let rand = Randoms.float rndgen 1.0 in
+    Misc.expo ~rand ~lambda
+  in
+  make_counting_trafficsource time_to_next_pkt num_pkts
+    
---
+++ sim/tsource.mli
@@ -0,0 +1,21 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
+(** Traffic Generators. 
+  @author Henri Dubois-Ferriere.
+*)
+
+type traffic_generator_t = (unit -> float option)
+    (** A traffic generator is a function that either returns 
+      - Some t, where t is the interval till the next packet to send.
+      - None, if there are no more packets to send*)
+
+
+val make_cbr : num_pkts:int -> pkts_per_sec:int -> traffic_generator_t
+  (** Returns a traffic_generator_t which will originate num_pkts, at a rate
+    of pkts_per_sec *)
+
+val make_poisson :  num_pkts:int -> lambda:float -> traffic_generator_t
+  (** Returns a traffic_generator_t which will originate num_pkts, spaced
+    with an exponential distribution of parameter lambda (mean 1/lambda) *)
