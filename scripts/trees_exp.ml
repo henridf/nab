a6350448493a84c4624217e576a6be0a91998fe3
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-09-04 02:47:35 +0000

    *** empty log message ***

--- scripts/trees_exp.ml
+++ scripts/trees_exp.ml
@@ -2,7 +2,7 @@ let durations = [1200]
 let sinks = [1;2;3;4;5;6;7;8;9;10]
 let runs = [1; 2; 3; 4; 5]
 let difftypes = ["voronoi"; "opp"; "ess"]
-let mactypes = ["nullmac"; "contmac"]
+let mactypes = ["nullmac"]
 let tmpfile = "/tmp/out.txt"
 let resfile = "./out.txt"
   
