f2895f6b5ed64da57d6a8cf95faa463b2fff7d6c
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-11-12 10:18:00 +0000

    change the name 'init_seed' to rewind seed. allow user to pass an optional value to change_seed.
    redo the docs

--- misc/randoms.ml
+++ misc/randoms.ml
@@ -7,17 +7,20 @@
 let initialseed = 12
 
 let seed = ref initialseed
-let change_seed() = (
-  seed := !seed + 14;
+let change_seed ?newseed () = (
+  begin 
+    match newseed with 
+      | None ->  seed := !seed + 14;
+      | Some s -> seed := !seed + s;
+  end;
   Random.init !seed
 )
 
-let init_seed() = (
-  seed := initialseed;
+let rewind_seed() = 
   Random.init !seed
-)
 
-type handle = {mutable state: Random.state}
+
+type rng_stream = {mutable state: Random.state}
 
 let save_state_ handle = 
   handle.state<-Random.get_state()
@@ -27,22 +30,20 @@ let create ?seed () =
     | None -> {state=Random.get_state()}
     | Some s -> Random.init s; {state=Random.get_state()}
 	
-  
-let int handle i = 
+let stream_ handle f = 
+  let s = Random.get_state () in
   Random.set_state handle.state; 
-  let number = Random.int i in
+  let number = f() in
   save_state_ handle;
+  Random.set_state s;
   number
 
+let int handle i =
+  stream_ handle (fun () -> Random.int i) 
   
-let float handle float = 
-  Random.set_state handle.state; 
-  let number = Random.float float in
-  save_state_ handle;
-  number
+let float handle x = 
+  stream_ handle (fun () -> Random.float x) 
 
 let bool handle = 
-  Random.set_state handle.state; 
-  let bool = Random.bool () in
-  save_state_ handle;
-  bool
+  stream_ handle (fun () -> Random.bool ()) 
+
--- misc/randoms.mli
+++ misc/randoms.mli
@@ -2,31 +2,52 @@
 (* mws  multihop wireless simulator *)
 (*                                  *)
 
-(** Wrappers around the standard library Random module, to de-multiplex
-  multiple separate RNG streams out of a single source.
+(** Wrappers around the standard library Random module, to allow 'rewind' in
+  the RNG stream and de-multiplexing multiple separate RNG streams out of a
+  single source.
+
+  The first feature ('rewind') is useful for example if we have a simulation
+  script where we wish to re-run the same scenario (with identical mobility,
+  traffic pattern) many times, with a different protocol set up at each time. 
+  With this we can set the seed back to its starting value and be sure that we
+  are testing each protocol over exactly the same inputs.
+
+  The second feature ('demultiplex') is useful for example so that introducing a
+  new component to an existing simulation script does not 'perturb' the
+  random number sequence when this component requires random numbers. 
+
   @author Henri Dubois-Ferriere.
 *)
 
-val seed : int ref
-  (** Value last used to seed the RNG (with Random.init) *)
+(** {2 Global RNG} *)
 
-val change_seed : unit -> unit
+val change_seed : ?newseed:int -> unit -> unit
   (** Change the RNG seed. *)
 
-val init_seed : unit -> unit
-  (** Change the RNG seed back to the initial value (the one that this starts
-    off with). This would be useful for example when we have run a bunch of
-    GREP tests and now want to start all over again and run a bunch of AODV tests.*)
+val rewind_seed : unit -> unit
+  (** Bring the RNG seed back to the value it was given in the last call to
+    {!Randoms.rewind_seed}, or it's initial value if {!Randoms.rewind_seed} has never been called. *)
 
 
 
-type handle
+(** {2 RNG Streams} *)
 
-val create : ?seed:int -> unit -> handle
+type rng_stream
+  (** A type used as a handle for an individual RN stream *)
 
-val int : handle -> int -> int
+val create : ?seed:int -> unit -> rng_stream
+  (** Create a stream. Starting seed is optional; if not provided an
+    unspecified seed is chosen *) 
 
-val bool : handle -> bool
+val int : rng_stream -> int -> int
+  (** Returns a random integer from the given stream. See Random.int from the
+    standard library. *)
 
-val float : handle -> float -> float
+val bool : rng_stream -> bool
+  (** Returns a random boolean from the given stream. See Random.bool from the
+    standard library. *)
+
+val float : rng_stream -> float -> float
+  (** Returns a random float from the given stream. See Random.float from the
+    standard library. *)
 
