5d6179d1bec4f51d31422c93205f1008d6296a11
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2003-11-17 16:00:02 +0000

    flesh out NaryTree implementation

--- misc/naryTree.ml
+++ misc/naryTree.ml
@@ -2,16 +2,51 @@ type 'a t = Empty | Node of 'a * 'a t list
 
 let rec belongs el =  function
   | Empty -> false
-  | Node (v, t) -> (v = el) || (List.exists (belongs el) t)
+  | Node (parent, children) -> (parent = el) || (List.exists (belongs el) children)
 
 let rec height = 
   let max_list l = List.fold_left max 0 l in
   function 
     | Empty -> 0
-    | Node (_, t) -> 1 + (max_list (List.map height t))
+    | Node (_, children) -> 1 + (max_list (List.map height children))
 
-let rec iter f = 
+let rec iter ~f = 
   function
     | Empty -> ()
-    | Node (el, t) -> f el; List.iter (iter f) t
+    | Node (parent, children) -> f parent; List.iter (iter ~f) children
+
+let iter2 ~f tree = 
+  let myf ~parent ~child = 
+    match child with
+      | Empty -> ()
+      | Node (c, _) -> f ~parent ~child:c
+  in
+  let rec iter2_ = 
+    function
+    | Empty -> ()
+    | Node (parent, children) -> List.iter (fun child -> myf ~parent ~child) children; List.iter iter2_ children
+  in iter2_ tree
+  
+
+let root = function
+  | Empty -> failwith "root"
+  | Node (v, _) -> v
+
+	
+let rec map ~f = 
+  function 
+    | Empty -> Empty
+    | Node (parent, children) -> Node (f parent, List.map (map ~f) children)
+	
+let addnode ~parent ~node tree = 
+  if belongs node tree then failwith "addnode";
+  let addtolist l = (Node (node, []))::l in
+  
+  let rec add_ = function 
+    | Empty -> Node (node, [])
+    | Node (v, subtree) when (v = parent) ->
+	Node (v, addtolist subtree)
+    | Node (v, subtree) -> Node (v, List.map add_ subtree)
+  in
+  add_ tree 
 
--- misc/naryTree.mli
+++ misc/naryTree.mli
@@ -1,9 +1,32 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
+(** Functional n-ary trees. *)
+
 type 'a t = Empty | Node of 'a * 'a t list
 
-(* val map ('a -> 'b) -> 'a naryTree -> 'b naryTree *)
+val map : f:('a -> 'b) -> 'a t -> 'b  t
 
 val belongs : 'a -> 'a t -> bool
 val height :  'a t -> int
 
+val root : 'a t -> 'a
+  (** Returns the node at the root of the tree. Raise [Failure "root"] if tree
+    is empty.*)
+
+val iter : f:('a -> unit) -> 'a t -> unit
+  (** Iterate over tree, presenting each node once to the provided function. 
+    Order is not specified. *)
+
+val iter2 : f:(parent:'a -> child:'a -> unit) -> 'a t -> unit
+  (** Iterate over tree, presenting each node along with its parent to the
+    provided function. Order is not specified *)
+
+
+val addnode : parent:'a -> node:'a -> 'a t -> 'a t 
+  (** [NaryTree.addnode ~parent ~node tree] returns a new tree which is the
+    result of adding [node] under [parent] in [tree]. Raise [Failure "nth"] if
+    [parent] is not in [tree].*)
 
 
