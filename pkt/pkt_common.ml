f46691c61daaa0101af0b9277a4f33025387650c
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-02-03 17:22:44 +0000

    new file containing common declarations for all packet-related modules.

---
+++ pkt/pkt_common.ml
@@ -0,0 +1,13 @@
+(*                                  *)
+(* mws  multihop wireless simulator *)
+(*                                  *)
+
+(** Common declarations for all packet-related modules.
+
+  @author Henri Dubois-Ferriere.
+*)
+
+let _ADDR_SIZE = 4
+let _TTL_SIZE = 1
+let _SEQNO_SIZE = 4
+let _FLOAT_SIZE = 8
