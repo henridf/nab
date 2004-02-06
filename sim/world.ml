5c9ab6875185438825cceba37215f5ad40bfb25d
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-02-06 10:44:12 +0000

    - split out class world type into separate file (preparation for directory   reorg). - explicitly separate world type between lazy_world and greedy_world (since   these differ by one method) rather than have this method be in both and   raise an exception in the class which doesn't implement it. - therefore global accessors now explicitly differentiate between global lazy   or greedy world objects (of course there should only be one).

--- sim/world.ml
+++ sim/world.ml
@@ -3,99 +3,20 @@
 (*                                  *)
 
 
-open Misc
-
 open Common
 
-class type world_t = 
-object
-    
-  method random_pos : Coord.coordf_t 
-    (** Return a uniform random position over the surface of the simulated area *)
-
-  method get_nodes_within_radius :  nid:Common.nodeid_t -> radius:float -> Common.nodeid_t list
-    (** Return all nodes within a given distance of a node.*)
-
-  method dist_coords :  Coord.coordf_t -> Coord.coordf_t -> float
-    (** Compute distance between two euclidean coordinates. *)
-
-  method dist_nodeids :  Common.nodeid_t -> Common.nodeid_t -> float
-    (** Compute distance between two nodes. *)
-    
-  method neighbors : Common.nodeid_t -> Common.nodeid_t list
-    (** Return all the neighbors of a node. Neighbors are nodes within
-      distance {!Params.rrange} of each other. *)
-
-  method are_neighbors : Common.nodeid_t -> Common.nodeid_t -> bool
-    (** Return true if two nodes are neighbors, false otherwise.
-      Neighbors are nodes within  distance {!Params.rrange} of each other. *)
-
-  method add_new_ngbr_hook : Common.nodeid_t -> hook:(Common.nodeid_t -> unit) -> unit
-    (** Any agent (routing, application, etc) who wants to know when nodes
-      enter the neighborhood of the node given as first argument should
-      register through this hook. 
-    *)
-
-  method add_mob_mhook : hook:(Coord.coordf_t -> Common.nodeid_t -> unit) -> unit
-    (** Any monitoring application can register here to receive update each
-       time the node moves.
-       If multiple apps, order in which called is unspecified.*)
-    
-  method init_pos : nid:Common.nodeid_t -> pos:(Coord.coordf_t) -> unit
-    (** Set initial position of a node. This should only be used once, after a
-      node object has been created. *)
-
-  method movenode : 
-    nid:Common.nodeid_t -> 
-    newpos:Coord.coordf_t ->
-    unit
-    (**  Each time a node moves, the World.object should be informed through
-      this method.
-    *)
-
-  method nodepos : Common.nodeid_t -> Coord.coordf_t 
-    (** Return the position of a node. *)
-
-  method find_closest : pos:Coord.coordf_t -> f:(Common.nodeid_t -> bool) ->
-    Common.nodeid_t option
-    (** Returns the closest node to pos which satisfies the boolean f, or None 
-      if f never satisfied. 
-      A common use is to pass the function [~f:(fun _ -> true)] in order to
-      find the node closest to a point.
-    *)
-
-    
-  method is_connected : unit -> bool
-    (** Returns true if the network is connected (under a boolean connectivity
-      model, with connectivity up to Params.rrange), false otherwise.
-      Warning: this is SLOW for networks in the 100s of nodes or above. *)
-
-  (**/**)
-
-  method neighbors_consistent : bool
-
-  method boundarize : Coord.coordf_t -> Coord.coordf_t
-    (* A topology-specific function which should put a point back within the
-       boundaries of the World. if it has stepped outside, and should return
-       the point intact if it has not.
-       For example this might be implemented as wrapping in a torus, or
-       bouncing on reflective borders, etc 
-       This allows a mobility process computing a next position to be unaware
-       of the topology limits. *)
-    
-  method project_2d : Coord.coordf_t -> Coord.coordf_t 
-    (* return position projected to a unit square *)
-    
-  (*  method scale_unit : float -> float 
-      return coordinate scaled to [0;1] interval. 
-      can be used for example for drawing search disks *)
-end
-  
 
 open Misc
 
-let (world_:world_t option ref) = ref None 
-let w () = try o2v !world_ with
+let (lazy_world_ : Worldt.lazy_world_t option ref) = ref None 
+let w () = try o2v !lazy_world_ with
   | Failure _ -> raise (Failure "World.w() : no instance has been set")
+let set_lazy_world t = lazy_world_ := Some t
 
-let set_world t = world_ := Some t
+let (greedy_world_ : Worldt.greedy_world_t option ref) = ref None 
+let gw () = try o2v !greedy_world_ with
+  | Failure _ -> raise (Failure "World.gw() : no instance has been set")
+
+let set_greedy_world t = 
+  greedy_world_ := Some t; 
+  lazy_world_ := Some (t :> Worldt.lazy_world_t)
--- sim/world.mli
+++ sim/world.mli
@@ -2,108 +2,19 @@
 (* mws  multihop wireless simulator *)
 (*                                  *)
 
-(** 
-  World: All computation requiring omniscience is done here. 
-  Things like:
-  - finding neighbors of a node
-  - finding the nearest node to a point satisfying some property
-  - computing a delaunay triangulation (not implemented)
-  - computing the shortest path between two nodes
-  - checking connectedness of a network.
 
-  @author Henri Dubois-Ferriere.
-*)
+(** Note that there can only be one world object *)
 
-open Misc
+val set_lazy_world : Worldt.lazy_world_t -> unit
+  (** Sets the global lazy world object. *)
 
-open Common
+val set_greedy_world : Worldt.greedy_world_t -> unit
+  (** Sets the global lazy world object. *)
 
-class type world_t = 
-object
-    
-  method random_pos : Coord.coordf_t 
-    (** Return a uniform random position over the surface of the simulated area *)
+val w : unit -> Worldt.lazy_world_t
+  (** Returns the global lazy_world object, if one has been set. 
+    Otherwise, returns the global greedy_world object, coerced to a
+    lazy_world, if a greedy_world has been set. *)
 
-  method dist_coords :  Coord.coordf_t -> Coord.coordf_t -> float
-    (** Compute distance between two euclidean coordinates. *)
-
-  method dist_nodeids :  Common.nodeid_t -> Common.nodeid_t -> float
-    (** Compute distance between two nodes. *)
-    
-  method neighbors : Common.nodeid_t -> Common.nodeid_t list
-    (** Return all the neighbors of a node. Neighbors are nodes within
-      distance {!Params.rrange} of each other. *)
-
-  method are_neighbors : Common.nodeid_t -> Common.nodeid_t -> bool
-    (** Return true if two nodes are neighbors, false otherwise.
-      Neighbors are nodes within  distance {!Params.rrange} of each other. *)
-
-  method add_new_ngbr_hook : Common.nodeid_t -> hook:(Common.nodeid_t -> unit) -> unit
-    (** Any agent (routing, application, etc) who wants to know when nodes
-      enter the neighborhood of the node given as first argument should
-      register through this hook. 
-    *)
-
-  method add_mob_mhook : hook:(Coord.coordf_t -> Common.nodeid_t -> unit) -> unit
-    (** Any monitoring application can register here to receive update each
-       time the node moves.
-       If multiple apps, order in which called is unspecified.*)
-    
-  method init_pos : nid:Common.nodeid_t -> pos:(Coord.coordf_t) -> unit
-    (** Set initial position of a node. This should only be used once, after a
-      node object has been created. *)
-
-  method movenode : 
-    nid:Common.nodeid_t -> 
-    newpos:Coord.coordf_t ->
-    unit
-    (**  Each time a node moves, the world object should be informed through
-      this method.
-    *)
-
-  method nodepos : Common.nodeid_t -> Coord.coordf_t 
-    (** Return the position of a node. *)
-
-  method find_closest : pos:Coord.coordf_t -> f:(Common.nodeid_t -> bool) ->
-    Common.nodeid_t option
-    (** Returns the closest node to pos which satisfies the boolean f, or None 
-      if f never satisfied. 
-      A common use is to pass the function [~f:(fun _ -> true)] in order to
-      find the node closest to a point.
-    *)
-
-  method is_connected : unit -> bool
-    (** Returns true if the network is connected (under a boolean connectivity
-      model, with connectivity up to Params.rrange), false otherwise.
-      Warning: this is SLOW for networks in the 100s of nodes or above. *)
-
-  method get_nodes_within_radius :  nid:Common.nodeid_t -> radius:float -> Common.nodeid_t list
-    (** Return all nodes within a given distance of a node. 
-      Note: Current implementation is not efficient (iterates over all nodes).*)
-
-  (**/**)
-
-  method neighbors_consistent : bool
-
-  method boundarize : Coord.coordf_t -> Coord.coordf_t
-    (* A topology-specific function which should put a point back within the
-       boundaries of the world, if it has stepped outside, and should return
-       the point intact if it has not.
-       For example this might be implemented as wrapping in a torus, or
-       bouncing on reflective borders, etc 
-       This allows a mobility process computing a next position to be unaware
-       of the topology limits. *)
-    
-  method project_2d : Coord.coordf_t -> Coord.coordf_t 
-    (* return position projected to a unit square *)
-    
-  (*  method scale_unit : float -> float 
-      return coordinate scaled to [0;1] interval. 
-      can be used for example for drawing search disks *)
-end
-  
-val set_world : world_t -> unit
-  (** Sets the global world object. *)
-
-val w : unit -> world_t
-  (** Returns the global world object, if one has been set. *)
+val gw : unit -> Worldt.greedy_world_t
+  (** Returns the global greedy_world object, if one has been set. *)
