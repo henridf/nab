a99f86c2a423b81c3f1c4c133b09fb551f4c1be3
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-02-08 14:34:37 +0000

    remove LER specific types (they belong in Le_tab.ml)

--- sim/common.ml
+++ sim/common.ml
@@ -7,24 +7,11 @@ type nodeid_t = int
 type time_t = float
 
 
-type enc_t = {
-  mutable t: time_t; 
-  mutable p: Coord.coordf_t
-}
-    
 let time_ = ref 0.0
 
 let set_time t = time_ := t
 
 let get_time () = !time_
 let time = get_time
-
-
-let enc ~time ~place = {t = time; p = place}
-let enc_age  m = (get_time ()) -. m.t
-
-
-
-
 
 
--- sim/common.mli
+++ sim/common.mli
@@ -10,14 +10,6 @@ type nodeid_t = int
 
 type time_t = float
 
-type enc_t = {
-  mutable t: time_t; 
-  mutable p: coordf_t
-}
-
-val enc : time:time_t -> place:Coord.coordf_t -> enc_t
-val enc_age : enc_t -> time_t
-
 val set_time : time_t -> unit
 val get_time : unit -> time_t
 val time: unit -> time_t
