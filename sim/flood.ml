8bc5286867683f816a9ef9e433875819628ec3fa
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-02-10 11:33:56 +0000

    file reorg

--- sim/flood.ml
+++
@@ -1,8 +0,0 @@
-(*                                  *)
-(* mws  multihop wireless simulator *)
-(*                                  *)
-
-type t = Common.nodeid_t NaryTree.t
-let create root  = NaryTree.Node (root, [])
-let addnode = NaryTree.addnode
-let to_coords = NaryTree.map ~f:(fun n -> (World.w())#nodepos n)
--- sim/trace.mli
+++
@@ -1,9 +0,0 @@
-(*                                  *)
-(* mws  multihop wireless simulator *)
-(*                                  *)
-
-(* Tracing facilities. This is for spitting out naml-readable messages 
-   for interpreting by naml *)
-val set_trace_chan : out_channel -> unit
-val close_trace_chan : unit -> unit
-
