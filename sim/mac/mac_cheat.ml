59db60058db6962c71482dc63cf061652a3667ec
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-03-08 18:26:19 +0000

    add copyright notice

--- sim/mac/mac_cheat.ml
+++ sim/mac/mac_cheat.ml
@@ -1,3 +1,33 @@
+(*
+
+  Copyright (C) 2004 Swiss Federal Institute of Technology Lausanne (EPFL),
+  Laboratory of Audiovisual Communications (LCAV) and 
+  Laboratory for Computer Communications and Applications (LCA), 
+  CH-1015 Lausanne, Switzerland
+
+  Author: Henri Dubois-Ferriere 
+
+  This file is part of mws (multihop wireless simulator).
+
+  Mws is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  Mws is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with mws; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+*)
+
+
+
 (** 
   "Cheat" MAC Layer: A MAC layer which does not take into account connectivity
   range or neighborhood. Using the "cheat" MAC, one can directly send a packet
