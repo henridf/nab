373022e354bd1cd5f50b2e0b6d654e22a201e525
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-03-25 08:39:07 +0000

    remove copyright notice, these are in contrib!

--- lib/contrib/opt.ml
+++ lib/contrib/opt.ml
@@ -1,34 +1,4 @@
 (*
-
-  Copyright (C) 2004 Swiss Federal Institute of Technology Lausanne (EPFL),
-  Laboratory of Audiovisual Communications (LCAV) and 
-  Laboratory for Computer Communications and Applications (LCA), 
-  CH-1015 Lausanne, Switzerland
-
-  Author: Henri Dubois-Ferriere 
-
-  This file is part of mws (multihop wireless simulator).
-
-  Mws is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
-  Mws is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with mws; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-
-*)
-
-
-
-(*
  * Option - functions for the option type
  * Copyright (C) 2003 Nicolas Cannasse
  * 
