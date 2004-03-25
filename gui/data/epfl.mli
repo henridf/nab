29ddbf81737c52b822cedbb0996ef6594d51106a
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-03-25 14:32:46 +0000

    update copyright notice with new name (fake)

--- gui/data/epfl.mli
+++ gui/data/epfl.mli
@@ -1,31 +1,24 @@
 (*
-
-  Copyright (C) 2004 Swiss Federal Institute of Technology Lausanne (EPFL),
-  Laboratory of Audiovisual Communications (LCAV) and 
-  Laboratory for Computer Communications and Applications (LCA), 
-  CH-1015 Lausanne, Switzerland
-
-  Author: Henri Dubois-Ferriere 
-
-  This file is part of mws (multihop wireless simulator).
-
-  Mws is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
-  Mws is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with mws; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-
-*)
-
+ *
+ *  Fake - a network simulator
+ *  Henri Dubois-Ferriere, LCA/LCAV, EPFL
+ * 
+ *  Copyright (C) 2004 Laboratory of Audiovisual Communications (LCAV), and
+ *  Laboratory for Computer Communications and Applications (LCA), 
+ *  Ecole Polytechnique Federale de Lausanne (EPFL),
+ *  CH-1015 Lausanne, Switzerland
+ *
+ *  This file is part of fake. Fake is free software; you can redistribute it 
+ *  and/or modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation; either version 2 of the License,
+ *  or (at your option) any later version. 
+ *
+ *  Fake is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *  details (enclosed in the file GPL). 
+ *
+ *)
 
 
 val epfl_xpm : string array
