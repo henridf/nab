bc72bf65f7b32d78e5b98a7caff1bb58fde5690d
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2004-06-10 13:27:34 +0000

    fix bugs: - data packets going out of a node twice (ie when teh first transmission fails due to broken route) were causing the same hop to be repeated twice in the route.
    - don't panic when we get an RREQ packet from originator which is not
    originator of most recent RREQ (ie, an old RREQ packet floating around).
    
    - don't panic when we get an RREQ from a previous route establishment.

--- proto/aodv/od_hooks.ml
+++ proto/aodv/od_hooks.ml
@@ -26,12 +26,13 @@ open Misc
 
 let routes_done = ref 0
  
-let find_last_flood route = 
+let find_last_flood route rreq_orig = 
   let n = ref None in
   for i = (List.length route) - 1 downto 0 do
-    if (List.nth route i).Route.info <> None then
-      if !n = None then
-	n := Some i
+    if (List.nth route i).Route.info <> None && 
+      NaryTree.root (o2v (Route.nth_hop route i).Route.info) = rreq_orig then
+	if !n = None then
+	  n := Some i
   done;
   !n
 
@@ -48,11 +49,29 @@ let pkt_type l3pkt =
 	  | Aodv_pkt.RREP_ACK -> `NONE end
     | `GREP_HDR h -> ((Grep_pkt.flags h) :> u)
     | `STR_HDR h -> 
-	begin match h.Str_pkt.ph with 
+	begin match h with 
 	  | Str_pkt.DATA _ -> `DATA
 	  | Str_pkt.RREQ _ -> `RREQ
 	  | Str_pkt.RREP _ -> `RREP
 	  | Str_pkt.HELLO -> `RADV end
+    | _ -> failwith "Od_hooks.pkt_type : unknown packet type"
+
+let rreq_orig l3pkt = 
+  match L3pkt.l3hdr_ext l3pkt with 
+    | `AODV_HDR h -> 
+	begin match h with 
+
+	  | Aodv_pkt.RREQ r -> r.Aodv_pkt.rreq_orig
+	  | Aodv_pkt.RREP _
+	  | Aodv_pkt.RERR _
+	  | Aodv_pkt.DATA  | Aodv_pkt.RREP_ACK -> failwith "Od_hooks.rreq_orig" end
+    | `GREP_HDR h -> failwith "Od_hooks.rreq_orig" 
+    | `STR_HDR h -> 
+	begin match h with 
+	  | Str_pkt.RREQ r -> r.Str_pkt.rreq_orig
+	  | Str_pkt.DATA _
+	  | Str_pkt.RREP _
+	  | Str_pkt.HELLO -> failwith "Od_hooks.rreq_orig" end 
     | _ -> failwith "Od_hooks.pkt_type : unknown packet type"
 
 let rreq_orig l3pkt = 
@@ -65,7 +84,7 @@ let rreq_orig l3pkt =
       end
     | `GREP_HDR h -> L3pkt.l3src l3pkt
     | `STR_HDR h -> begin 
-	match h.Str_pkt.ph with
+	match h with
 	  | Str_pkt.RREQ rreq -> rreq.Str_pkt.rreq_orig
 	  | Str_pkt.RREP _ | Str_pkt.HELLO 
 	  | Str_pkt.DATA _ -> failwith "Od_hooks.rreq_orig : not a rreq"
@@ -75,34 +94,60 @@ let rreq_orig l3pkt =
 let od_route_pktin_mhook routeref l2pkt node = (
   
   let l3pkt = (L2pkt.l3pkt l2pkt) in
-  let l3dst = L3pkt.l3dst l3pkt
-
+  let l3dst, l3src = L3pkt.l3dst l3pkt, L3pkt.l3src l3pkt
   and l2src = (L2pkt.l2src l2pkt) in
 
   if (l2src = node#id) then failwith "Od_hooks.od_route_pktin_mhook";
 
   match pkt_type l3pkt with
     | `DATA ->
-	Log.log#log_debug (lazy (Printf.sprintf "Arriving at node %d" node#id));	  
-	if  node#id = l3dst then ( (* Packet arriving at dst. *)
-	  incr routes_done;
+	Log.log#log_info (lazy (Printf.sprintf "Od_hooks: DATA Arriving at node %d" node#id));	  
+	
+	if Route.length !routeref = 0 then (*l2src = l3src && (Route.last_hop !routeref).Route.hop <> l3src then *)
+	  (* add originator if packet leaving src.
+	     2nd test is in case there is a packet loop, the packet goes back
+	     through the source, to prevent adding the source twice at this point.
+	  *)
 	  routeref := Route.add_hop !routeref {
-	    Route.hop=node#id;
+	    Route.hop=l3src;
 	    Route.info=None
-	  }
+	  };
+	routeref := Route.add_hop !routeref {
+	  Route.hop=node#id;
+	  Route.info=None
+	};
+	if  node#id = l3dst then ( (* Packet has arrived. *)
+	  incr routes_done;
 	)
+
     | `RREQ  ->
-	assert (Route.length !routeref > 0);
-	let hopno = find_last_flood !routeref in
-	(* xxx this seems to implicitly assume that all RREQ flood packets are
-	   from the most recently originated flood - always true? *)
-	assert (hopno <> None);
+	let rreq_orig = rreq_orig l3pkt in
+(*
+  assert (Route.length !routeref > 0);
+  this assert might fail if there are old rreq packets from a previos route
+  lying around*)
+
+	let hopno = find_last_flood !routeref rreq_orig in
+	(*
+	  assert (if hopno = None then (
+	  Log.log#log_error (lazy 
+	  (Printf.sprintf "No previous flood for originator %d" rreq_orig));
+	  false) else true);
+	  
+	  if we get a RREQ packet from an originator who is not on the route
+	  we're building, assume that it's some leftover form a previous route
+	  discovery and don't do anything.
+	*)
+	if hopno <> None then 
 	let tree = o2v (Route.nth_hop !routeref (o2v hopno)).Route.info in
 	Log.log#log_debug (lazy 
 	  (Printf.sprintf "packet arriving at %d with l2src %d" node#id l2src));	
 (*	Log.log#log_info (lazy (NaryTree.sprintf ~f:string_of_int tree));*)
 
-	assert (NaryTree.belongs l2src tree);
+	assert (if not (NaryTree.belongs l2src tree) then (
+	  Log.log#log_error (lazy (NaryTree.sprintf ~f:string_of_int tree));
+	  false ) else true
+	);
 
 	let newtree = 
 	  (* A real flood in the network is not a tree, so this node may
@@ -126,15 +171,13 @@ let od_route_pktout_mhook routeref l2pkt node = (
   if (l2src <> node#id) then failwith "Od_hooks.od_route_pktout_mhook";
   
   match pkt_type l3pkt with
-    | `DATA ->
-	Log.log#log_info (lazy (Printf.sprintf "Leaving node %d" node#id));	
-	routeref := Route.add_hop !routeref {
-	  Route.hop=node#id;
-	  Route.info=None
-	}
-	  
+    | `DATA -> ()
+	(* don't add hop when  packet leaving src, because it might leave
+	   source twice (when we have a broken next hop. *)
     | `RREQ when (rreq_orig l3pkt = l2src) ->	(* RREQ leaving initiator *)
 	begin	
+	  Log.log#log_info (lazy (Printf.sprintf "Od_hooks: %d originating RREQ" node#id));	  
+
 	  let orig = rreq_orig l3pkt in
 	  match Route.length !routeref with
 	      (* Add hop if this node is not yet on the route 
