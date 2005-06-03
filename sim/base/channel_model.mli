dbe1550881579baa795aa32344473687e444cb93
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2005-06-03 08:30:35 +0000

    doc (by t. schmid)

--- sim/base/channel_model.mli
+++ sim/base/channel_model.mli
@@ -23,13 +23,20 @@
 (* $Id$ *)
 
 (** Channel model. 
-  xxx/comment [ overview of what this file provides ]
+
+    This file provides a shadowing channel model and different radio channel
+    measurements. The gamma function provides a simple way to calculate the
+    SNR at the receiver. pe calculates
+    the bit error probability and error_probability the probability that a whole
+    message is received correctly at the receiver, given a certain encoding.
+
   @author Thomas Schmid.
 *)
 
 
 (** RF channel parameters from "Near Ground Wideband Channel
-  Measurements"  xxx/comment where do thes come from? *)
+  Measurements", K. Sohrabi, B. Manriquez, G. Pottie.
+*)
 type rf_environment = 
  | Engineering_1    (** Engineering I *)
  | Appt_hallway     (** Appartment Hallway *)
@@ -48,21 +55,30 @@ type rf_environment =
 
 val gamma :  rf:rf_environment ->  p_t:float -> d0:float -> p_n:float -> 
   d:float -> float
-  (** Calculates gamma from the given parameters 
-    xxx/comment all parameters must be described! (at least one/two words to
-    give the name, ie is d 'distance' ? ).
-    and what is 'gamma' ?
+  (** Calculates gamma (received SNR and receiver) in [dB] from the given parameters 
+
+    [rf]: Type of channel model to use (see {!Channel_mode.rf_environment}).
+    [p_t]: Transmit power
+    [d0]: Distance at which PL(d0), i.e. the path loss at distance d0, has been 
+    calculated/ measured
+    [p_n]: Noise floor of the used radio chip
+    [d]: Distance between the sender and receiver in meters
   *)
   
 val pe : modulation:Radiochips.modulation_t -> gamma:float -> b_n:float -> rate:float -> float
   (** Calculates the bit error probability from the given parameters. 
-    xxx/comment parameters ;)
+      [gamma]: Receiver SNR in dB
+      [b_n]: Noise bandwith
+      [rate]: Data rate at which one is communicating
 *)
 
 val packet_reception_probability :
   encoding:Radiochips.phy_encoding_t ->
   framelength:int -> preamblelength:int -> pe:float -> float
   (** Calculates the probability of packet reception for the given
-    parameters. 
-    xxx/comment are the lengths in bytes or bits?
+    parameters.
+    [encoding]: Used encoding in the radio chip
+    [framelength]: length of framein byte
+    [preamblelength]: length of preamble (in bytes)
+    [pe]: bit error probability
   *)
