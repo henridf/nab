17449a40393f475965afa766f8805f858b6786d3
henridf <henridf@5e8c0ee2-f522-0410-974c-db354172515d>
2005-08-16 13:02:18 +0000

    use lcav, not lcmpc4

--- tools/release/release_commands.sh
+++ tools/release/release_commands.sh
@@ -2,19 +2,19 @@
 
 # TAG format release_nab_0_6pre_2004_03_29
 
-release=nab-0.6-1
-tag=release_nab-0-6-1_2004-04-05
+release=nab-0.7 # then -1 for sub-releases, if any
+tag=release_nab-0-7-1_2005-08-16
 cd wherever the source tree is
 cvs tag $tag
 rm -rf /tmp/nab
 cd /tmp
-cvs -d :ext:henridf@lcavsun10:/home/anoncvs/camlrepo export -r $tag nab
+cvs -d :ext:henridf@lcav.epfl.ch:/home/anoncvs/camlrepo export -r $tag nab
 rm nab/release/announce.txt
 
 tar cvf $release.tar nab
 gzip $release.tar 
 cp $release.tar.gz /home/henridf/archives
-scp $release.tar.gz henridf@lcmpc4.epfl.ch:/dir/lcavwww/nab/doc
-ssh lcmpc4.epfl.ch
+scp $release.tar.gz henridf@lcav.epfl.ch:/dir/lcavwww/nab/doc
+ssh lcav.epfl.ch
 #run work/release/update-nab.sh from there
 # don't forget to update pointer on download.html
